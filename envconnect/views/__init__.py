3557c722395ccbf17f81646ac93563484d918316
Sebastien Mirolo <smirolo@djaodjin.com>
2017-06-15 23:55:32 -0700

    redirects when manager of site

--- envconnect/views/__init__.py
+++ envconnect/views/__init__.py
@@ -4,6 +4,7 @@
 import logging
 
 from django import http
+from django.conf import settings
 from django.core.urlresolvers import reverse
 from deployutils.redirects import AccountRedirectView as AccountRedirectBaseView
 from survey.models import Response
@@ -19,3 +20,10 @@ LOGGER = logging.getLogger(__name__)
 class AccountRedirectView(ReportMixin, AccountRedirectBaseView):
 
     redirect_roles = ['manager', 'contributor']
+
+    def get(self, request, *args, **kwargs):
+        if self.manages(settings.APP_NAME):
+            kwargs.update({self.slug_url_kwarg: settings.APP_NAME})
+            return http.HttpResponseRedirect(
+                self.get_redirect_url(*args, **kwargs))
+        return super(AccountRedirectView, self).get(request, *args, **kwargs)
