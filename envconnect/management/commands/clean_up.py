fdf939fcebe69175e2958f79943d2a6d84e3ebc8
Sebastien Mirolo <smirolo@djaodjin.com>
2017-07-07 14:22:59 -0700

    adds --delete flag to command

--- envconnect/management/commands/clean_up.py
+++ envconnect/management/commands/clean_up.py
@@ -34,7 +34,15 @@ INDUSTRIES = [
 
 class Command(BaseCommand):
 
+    def add_arguments(self, parser):
+        super(Command, self).add_arguments(parser)
+        parser.add_argument('--delete', action='store_true',
+            dest='do_delete', default=False,
+            help='When set will also issue the delete query.')
+
     def handle(self, *args, **options):
+        #pylint:disable=too-many-locals
+        do_delete = options['do_delete']
         entry_points = INDUSTRIES \
             + ['basic-%s' % ind for ind in INDUSTRIES]
         roots = PageElement.objects.filter(slug__in=entry_points)
@@ -54,14 +62,16 @@ class Command(BaseCommand):
             self.stdout.write("DELETE FROM envconnect_consumption"\
                 " WHERE question_id in %s;\n" % str(tuple([consumption.pk
                 for consumption in consumptions_not_linked])))
-            #consumptions_not_linked.delete()
+            if do_delete:
+                consumptions_not_linked.delete()
             questions_not_linked = Question.objects.filter(
                 pk__in=[question.pk for question in consumptions_not_linked])
             self.stdout.write("-- %d questions are not linked\n"
                 % len(questions_not_linked))
             self.stdout.write("DELETE FROM survey_question WHERE id in %s;\n" %
                 str(tuple([question.pk for question in questions_not_linked])))
-            #questions_not_linked.delete()
+            if do_delete:
+                questions_not_linked.delete()
 
             # Remove PageElements which are not in entry_points
             pages_not_linked = [page for page in PageElement.objects.get_roots()
@@ -71,8 +81,9 @@ class Command(BaseCommand):
             self.stdout.write("DELETE FROM pages_pageelement WHERE id in %s;\n"
                 % str(tuple([page.pk for page in pages_not_linked])))
             self.stdout.write("COMMIT;\n")
-            #PageElement.objects.filter(
-            #    pk__in=[page.pk for page in pages_not_linked]).delete()
+            if do_delete:
+                PageElement.objects.filter(
+                    pk__in=[page.pk for page in pages_not_linked]).delete()
 
 
     def build_tree(self, root, prefix=None, indent=''):
