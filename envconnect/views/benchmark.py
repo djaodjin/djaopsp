8bc8c92d1d0db8fedd3cef28f5d61572222416a7
Sebastien Mirolo <smirolo@djaodjin.com>
2017-09-09 15:21:10 -0700

    Two columns of graphs instead of 3 (#57)
    Note that we artificially delay taking a screenshot to allow
    the Javascript animation to complete.

--- envconnect/views/benchmark.py
+++ envconnect/views/benchmark.py
@@ -300,7 +300,7 @@ casper.viewport(%(width)s, %(height)s).thenOpen('%(url)s', function() {
                     js_content=js_content,
                     cache_storage=cache_storage,
                     on_start=on_start,
-                    width=210, height=204)
+                    width=250, height=204, delay=1000)
             on_start = False
         js_content.write("""
 casper.run();
