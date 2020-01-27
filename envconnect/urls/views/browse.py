ba3dbfd7d3c96d5877fe343d41f51f29ffb3af85
Sebastien Mirolo <smirolo@djaodjin.com>
2020-01-27 13:25:06 -0800

    adds workflow for implicit grant

--- envconnect/urls/views/browse.py
+++ envconnect/urls/views/browse.py
@@ -1,53 +1,54 @@
-# Copyright (c) 2019, DjaoDjin inc.
+# Copyright (c) 2020, DjaoDjin inc.
 # see LICENSE.
 
+from deployutils.apps.django.templatetags.deployutils_prefixtags import (
+    site_prefixed)
 from pages.settings import PATH_RE
 from urldecorators import include, url
 
-from ...urlbuilders import APP_PREFIX
 from ...views.redirects import AccountRedirectView, MyTSPRedirectView
 from ...views.detail import DetailView, DetailXLSXView
 
 urlpatterns = [
 
     # authenticated user
-    url(r'app/info/portfolios(?P<path>%s)/' % PATH_RE,
+    url(r'app/portfolios(?P<path>%s)/' % PATH_RE,
         MyTSPRedirectView.as_view(
             pattern_name='matrix_chart',
-            new_account_url='/%sapp/new/' % APP_PREFIX),
+            new_account_url=site_prefixed('/users/roles/accept/')),
         name='envconnect_portfolio'),
-    url(r'app/info/requests/',
+    url(r'app/requests/',
         MyTSPRedirectView.as_view(
-            pattern_name='organization_reporting_entities',
-            new_account_url='/%sapp/new/' % APP_PREFIX),
+            pattern_name='organization_reporting_entities_base',
+            new_account_url=site_prefixed('/users/roles/accept/')),
         name='envconnect_share_requests'),
-    url(r'app/info/assess(?P<path>%s)/' % PATH_RE,
+    url(r'app/assess(?P<path>%s)/' % PATH_RE,
         AccountRedirectView.as_view(
             pattern_name='envconnect_assess_organization',
-            new_account_url='/%sapp/new/' % APP_PREFIX),
+            new_account_url=site_prefixed('/users/roles/accept/')),
                 name='envconnect_assess'),
-    url(r'app/info/improve(?P<path>%s)/' % PATH_RE,
+    url(r'app/improve(?P<path>%s)/' % PATH_RE,
         AccountRedirectView.as_view(
             pattern_name='envconnect_improve_organization',
-            new_account_url='/%sapp/new/' % APP_PREFIX),
+            new_account_url=site_prefixed('/users/roles/accept/')),
         name='envconnect_improve'),
-    url(r'app/info/benchmark(?P<path>%s)/'
+    url(r'app/benchmark(?P<path>%s)/'
         % PATH_RE,
         AccountRedirectView.as_view(
             pattern_name='benchmark_organization',
-            new_account_url='/%sapp/new/' % APP_PREFIX),
+            new_account_url=site_prefixed('/users/roles/accept/')),
         name='benchmark'),
-    url(r'app/info/scorecard(?P<path>%s)/'
+    url(r'app/scorecard(?P<path>%s)/'
         % PATH_RE,
         AccountRedirectView.as_view(
             pattern_name='scorecard_organization',
-            new_account_url='/%sapp/new/' % APP_PREFIX),
+            new_account_url=site_prefixed('/users/roles/accept/')),
         name='scorecard'),
-    url(r'app/info/share(?P<path>%s)/'
+    url(r'app/share(?P<path>%s)/'
         % PATH_RE,
         AccountRedirectView.as_view(
             pattern_name='envconnect_share_organization',
-            new_account_url='/%sapp/new/' % APP_PREFIX),
+            new_account_url=site_prefixed('/users/roles/accept/')),
         name='envconnect_share'),
     url(r'app/info(?P<path>%s)/download/' % PATH_RE,
       DetailXLSXView.as_view(), name='summary_download'),
