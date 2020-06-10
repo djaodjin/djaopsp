8dd08a075001dd89d1fce92d74a75d65a258b0c5
David Quiros <david@quiros.online>
2020-06-10 09:47:35 -0600

    Fix lint issues

--- client/web/supplier/src/main.js
+++ client/web/supplier/src/main.js
@@ -1,14 +1,14 @@
-import Vue from "vue";
-import VueCompositionApi from "@vue/composition-api";
+import Vue from 'vue'
+import VueCompositionApi from '@vue/composition-api'
 
-import App from "./App.vue";
-import router from "./router";
+import App from './App.vue'
+import router from './router'
 
-Vue.use(VueCompositionApi);
+Vue.use(VueCompositionApi)
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
   router,
   render: (h) => h(App),
-}).$mount("#app");
+}).$mount('#app')
--- client/web/supplier/src/views/Assess.vue
+++ client/web/supplier/src/views/Assess.vue
@@ -7,6 +7,6 @@
 
 <script>
 export default {
-  name: "home"
-};
+  name: 'assess',
+}
 </script>
