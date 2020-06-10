688cb5571acb8ddf8cc4cf6c77599c93f4d4d055
David Quiros <david@quiros.online>
2020-06-10 13:23:23 -0600

    Add app bar and nav drawer for home page

--- client/web/supplier/src/views/Home.vue
+++ client/web/supplier/src/views/Home.vue
@@ -1,5 +1,51 @@
 <template>
-  <hello-world />
+  <div>
+    <v-navigation-drawer v-model="drawer" app clipped>
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-compass</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Projects</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-cog</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Settings</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-help-circle</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Help</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-exit-to-app</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Sign out</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app clipped-left>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>TSP / Suppliers</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <hello-world />
+    </v-content>
+  </div>
 </template>
 
 <script>
@@ -7,6 +53,10 @@ import HelloWorld from '../components/HelloWorld'
 
 export default {
   name: 'Home',
+
+  data: () => ({
+    drawer: null,
+  }),
 
   components: {
     HelloWorld,
