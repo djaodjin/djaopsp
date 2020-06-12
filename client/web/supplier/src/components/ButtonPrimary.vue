5c1906991a4ed8304467951070b1ad464eca85a1
David Quiros <david@quiros.online>
2020-06-12 13:38:09 -0600

    Add content to HomeActions

---
+++ client/web/supplier/src/components/ButtonPrimary.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-btn :depressed="$vuetify.breakpoint.xs" block large color="primary">
+    <slot></slot>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: 'ButtonPrimary',
+}
+</script>
--- client/web/supplier/src/components/HomeActions.vue
+++ client/web/supplier/src/components/HomeActions.vue
@@ -1,15 +1,43 @@
 <template>
   <v-container>
-    <v-row class="text-center">
-      <v-col cols="12" sm="6" md="4">{{ $t('message') }}</v-col>
-      <v-col cols="12" sm="6" md="4">{{ $t('message') }}</v-col>
-      <v-col cols="12" sm="6" md="4">{{ $t('message') }}</v-col>
+    <v-row>
+      <v-col cols="12" md="6" lg="4">
+        <section class="mb-4 px-md-4">
+          <p>{{ $t('home.desc-assessment') }}</p>
+          <button-primary>{{ $t('home.btn-assessment') }}</button-primary>
+        </section>
+      </v-col>
+      <v-col cols="12" md="6" lg="4">
+        <section class="mb-4 px-md-4">
+          <p>{{ $t('home.desc-history') }}</p>
+          <button-primary>{{ $t('home.btn-history') }}</button-primary>
+        </section>
+      </v-col>
+      <v-col cols="12" md="6" lg="4">
+        <section class="mb-4 px-md-4">
+          <p>{{ $t('home.desc-best-practices') }}</p>
+          <button-primary>{{ $t('home.btn-best-practices') }}</button-primary>
+        </section>
+      </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import ButtonPrimary from '@/components/ButtonPrimary'
+
 export default {
   name: 'HomeActions',
+
+  components: {
+    ButtonPrimary,
+  },
 }
 </script>
+
+<style lang="scss" scoped>
+section {
+  max-width: 600px;
+  margin: 0 auto;
+}
+</style>
