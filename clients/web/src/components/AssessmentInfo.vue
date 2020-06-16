6132bd70c8fd3f659df0a82097df3a8f5058df54
David Quiros <david@quiros.online>
2020-06-16 17:59:47 -0600

    Create assessment card

--- clients/web/src/components/AssessmentInfo.vue
+++ clients/web/src/components/AssessmentInfo.vue
@@ -1,30 +1,32 @@
 <template>
-  <div>
-    <dl>
-      <dt>Industry</dt>
-      <dd>
-        <b>Engineering, Procurement, Construction</b>
-      </dd>
-      <dt>Created</dt>
-      <dd>
-        <time datetime="2020-06-15 10:34">June 15, 2020</time>
-      </dd>
-      <dt>Author</dt>
-      <dd>scott@tamerinsolutions.com</dd>
-    </dl>
-    <div v-if="isClickable">
-      <router-link :to="{ name: 'assessmentHome', params: { id: '123' } }">
-        Continue:
-        <b>Establish Current Practices</b>
-      </router-link>
-    </div>
-    <div v-else>
-      <span>
-        Status:
-        <b>Establish Current Practices</b>
-      </span>
-    </div>
-  </div>
+  <v-card class="mx-auto" max-width="340">
+    <v-card-text>
+      <ul>
+        <li>
+          <span>Industry:</span>
+          <b>Engineering, Procurement, Construction, Cows</b>
+        </li>
+        <li>
+          <span>Created:</span>
+          <time datetime="2020-06-15 10:34">June 15, 2020</time>
+        </li>
+        <li>
+          <span>Author:</span>
+          <em>scott@tamerinsolutions.com</em>
+        </li>
+        <li v-if="isClickable">
+          <router-link :to="{ name: 'assessmentHome', params: { id: '123' } }">
+            <span>Continue:</span>
+            <b>Establish Current Practices</b>
+          </router-link>
+        </li>
+        <li v-else>
+          <span>Status:</span>
+          <b>Establish Current Practices</b>
+        </li>
+      </ul>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
@@ -38,3 +40,26 @@ export default {
   }),
 }
 </script>
+
+<style lang="scss" scoped>
+li {
+  margin-bottom: 3px;
+
+  span {
+    display: inline-block;
+    margin-right: 2%;
+    width: 20%;
+    text-align: right;
+    vertical-align: top;
+
+    & + * {
+      display: inline-block;
+      width: 78%;
+      text-align: left;
+    }
+  }
+}
+em {
+  font-style: normal;
+}
+</style>
