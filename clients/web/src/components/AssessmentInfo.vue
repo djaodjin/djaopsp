c82608edd1c9d6b01109ec7bfab6c9d38495b606
David Quiros <david@quiros.online>
2020-06-17 13:10:52 -0600

    Make assessment info card responsive

--- clients/web/src/components/AssessmentInfo.vue
+++ clients/web/src/components/AssessmentInfo.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-card class="mx-auto" max-width="340">
-    <v-card-text>
+  <v-card class="mx-auto assessment-info">
+    <v-card-text class="pa-md-5">
       <ul>
         <li>
           <span>Industry:</span>
@@ -42,24 +42,37 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-li {
-  margin-bottom: 3px;
+.assessment-info {
+  max-width: 340px;
 
-  span {
-    display: inline-block;
-    margin-right: 2%;
-    width: 20%;
-    text-align: right;
-    vertical-align: top;
+  @media #{map-get($display-breakpoints, 'md-and-up')} {
+    max-width: 100%;
+  }
 
-    & + * {
+  li {
+    margin-bottom: 3px;
+
+    @media #{map-get($display-breakpoints, 'md-and-up')} {
+      font-size: 1rem;
+      margin-bottom: 5px;
+    }
+
+    span {
       display: inline-block;
-      width: 78%;
-      text-align: left;
+      margin-right: 2%;
+      width: 20%;
+      text-align: right;
+      vertical-align: top;
+
+      & + * {
+        display: inline-block;
+        width: 78%;
+        text-align: left;
+      }
     }
   }
-}
-em {
-  font-style: normal;
+  em {
+    font-style: normal;
+  }
 }
 </style>
