2109edd4bf9a5c2174c83461fbba951967500b75
David Quiros <david@quiros.online>
2020-06-19 11:19:32 -0600

    Build generic component for section tab navigation

--- clients/web/src/views/AssessmentEnvironmentalTargets.vue
+++ clients/web/src/views/AssessmentEnvironmentalTargets.vue
@@ -1,20 +1,35 @@
 <template>
   <fragment>
-    <section-title title="Environmental Targets" />
-    <span>Environmental Targets Here!</span>
+    <section-title :title="$t('targets.title')" />
+    <tab-container :tabs="tabs">
+      <template v-slot:tab1>Market Situation Content!</template>
+      <template v-slot:tab2>Environmental Targets Content!</template>
+    </tab-container>
   </fragment>
 </template>
 
 <script>
 import { Fragment } from 'vue-fragment'
 import SectionTitle from '@/components/SectionTitle'
+import TabContainer from '@/components/TabContainer'
 
 export default {
   name: 'AssessmentEnvironmentalTargets',
 
+  data() {
+    return {
+      tabs: [
+        { text: this.$t('targets.tab1-title'), href: 'tab-1' },
+        { text: this.$t('targets.tab2-title'), href: 'tab-2' },
+      ],
+      tab: null,
+    }
+  },
+
   components: {
     Fragment,
     SectionTitle,
+    TabContainer,
   },
 }
 </script>
