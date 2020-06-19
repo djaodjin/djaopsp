39043e999be5d90475ce90d0bce94a31bf3a7e0d
David Quiros <david@quiros.online>
2020-06-19 10:15:58 -0600

    Add responsive section content title

--- clients/web/src/components/SectionTitle.vue
+++ clients/web/src/components/SectionTitle.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="ml-4 mt-4 mb-6 ml-sm-8 mt-sm-8 mb-sm-10">
     <small>Environment Sustainability Assessment</small>
     <h1>{{ title }}</h1>
   </div>
@@ -12,3 +12,23 @@ export default {
   props: ['title'],
 }
 </script>
+
+<style lang="scss" scoped>
+h1 {
+  text-align: left;
+  font-size: 1.2rem;
+  font-weight: 500;
+  margin-top: 0;
+  line-height: 0.9;
+}
+
+@media #{map-get($display-breakpoints, 'sm-and-up')} {
+  h1 {
+    font-size: 1.8;
+    line-height: 1;
+  }
+  small {
+    font-size: 1rem;
+  }
+}
+</style>
