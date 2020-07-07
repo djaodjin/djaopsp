175756b5eefd165dded83397db87f40601e6a7ab
David Quiros <david@quiros.online>
2020-07-07 13:00:49 -0600

    Add tabs for improvement plan

--- clients/web/src/views/AssessmentImprovementPlan.vue
+++ clients/web/src/views/AssessmentImprovementPlan.vue
@@ -1,20 +1,43 @@
 <template>
   <fragment>
     <section-title title="Improvement Plan" />
-    <span>Improvement Plan Here!</span>
+    <tab-container :tabs="tabs">
+      <template v-slot:tab1>
+        <div class="pa-4">
+          <p>{{ $t('targets.tab1.intro') }}</p>
+        </div>
+      </template>
+      <template v-slot:tab2>
+        <div class="pa-4">
+          <p>{{ $t('targets.tab2.intro') }}</p>
+        </div>
+      </template>
+    </tab-container>
   </fragment>
 </template>
 
 <script>
 import { Fragment } from 'vue-fragment'
 import SectionTitle from '@/components/SectionTitle'
+import TabContainer from '@/components/TabContainer'
 
 export default {
   name: 'AssessmentImprovementPlan',
 
+  data() {
+    return {
+      tabs: [
+        { text: this.$t('targets.tab1.title'), href: 'tab-1' },
+        { text: this.$t('targets.tab2.title'), href: 'tab-2' },
+      ],
+      tab: null,
+    }
+  },
+
   components: {
     Fragment,
     SectionTitle,
+    TabContainer,
   },
 }
 </script>
