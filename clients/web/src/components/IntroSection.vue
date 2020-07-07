6b0be06bd8f8641702a938a8a6969ea087287cba
David Quiros <david@quiros.online>
2020-07-07 11:15:52 -0600

    Make number of colums a prop

--- clients/web/src/components/IntroSection.vue
+++ clients/web/src/components/IntroSection.vue
@@ -1,11 +1,11 @@
 <template>
   <v-container class="mt-sm-10" tag="section">
     <v-row justify="center">
-      <v-col cols="11" md="6" lg="5" tag="header" class="text-center">
+      <v-col :cols="cols" md="6" lg="5" tag="header" class="text-center">
         <small>Environment Sustainability Assessment</small>
         <h1 class="assessment-title">{{ title }}</h1>
       </v-col>
-      <v-col cols="11" md="6" lg="5">
+      <v-col :cols="cols" md="6" lg="5">
         <slot></slot>
       </v-col>
     </v-row>
@@ -16,6 +16,15 @@
 export default {
   name: 'IntroSection',
 
-  props: ['title'],
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    cols: {
+      type: Number,
+      default: 11,
+    },
+  },
 }
 </script>
