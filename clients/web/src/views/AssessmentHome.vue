ff8593ae73c312244e8803742ad8a56c9b0479d6
David Quiros <david@quiros.online>
2020-07-16 23:19:13 -0600

    Load/pass assessment to sub-components

--- clients/web/src/views/AssessmentHome.vue
+++ clients/web/src/views/AssessmentHome.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="assessment-container">
     <h1 class="assessment-title">Environment Sustainability Assessment</h1>
-    <v-container>
+    <v-container v-if="assessment">
       <v-row justify="center">
         <v-col cols="12" sm="8" md="6" lg="5">
-          <assessment-info />
+          <assessment-info :assessment="assessment" />
         </v-col>
         <v-col cols="12" sm="8" md="5">
-          <assessment-stepper />
+          <assessment-stepper :assessment="assessment" />
         </v-col>
       </v-row>
     </v-container>
@@ -15,13 +15,28 @@
 </template>
 
 <script>
+import { getAssessment } from '../mocks/assessments'
 import AssessmentInfo from '@/components/AssessmentInfo'
 import AssessmentStepper from '@/components/AssessmentStepper'
 
 export default {
   name: 'AssessmentHome',
 
-  data: () => ({}),
+  created() {
+    this.fetchData()
+  },
+
+  methods: {
+    async fetchData() {
+      this.assessment = await getAssessment(this.$route.params.id)
+    },
+  },
+
+  data() {
+    return {
+      assessment: null,
+    }
+  },
 
   components: {
     AssessmentInfo,
