726cf5ba41235510c180e44f9155fc86c29b1f71
David Quiros <david@quiros.online>
2020-07-17 23:08:39 -0600

    Remove usage of store and vuex

--- clients/web/src/main.js
+++ clients/web/src/main.js
@@ -1,19 +1,15 @@
 import Vue from 'vue'
 import VueCompositionApi from '@vue/composition-api'
-import Vuex from 'vuex'
 
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 import i18n from './plugins/i18n'
-import storeConfig from './config/store'
 
 Vue.use(VueCompositionApi)
 Vue.use(Vuex)
 
 Vue.config.productionTip = false
-
-const store = new Vuex.Store(storeConfig)
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -23,6 +19,5 @@ new Vue({
   router,
   vuetify,
   i18n,
-  store,
   render: (h) => h(App),
 }).$mount('#app')
