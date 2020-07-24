ce79e2afc18b9f9286b4673850e4daf9e2a7f6db
David Quiros <david@quiros.online>
2020-07-23 18:13:26 -0600

    Update mock industry lists to reflect API response

--- clients/web/src/mocks/industry-segments.js
+++ clients/web/src/mocks/industry-segments.js
@@ -1,126 +1,41 @@
 import { DELAY } from './config'
 
-export function getIndustrySegments() {
+export async function getIndustrySegments() {
+  // try {
+  //   const response = await fetch('/envconnect/api/content/campagins?q=public')
+  //   const data = await response.json()
+  //   console.log(data)
+  // } catch (e) {
+  //   console.error('Ooops', e)
+  // }
+
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve([
-        {
-          text: 'Architectural design',
-          value: 'architecture-design',
-        },
-        {
-          text: 'Aviation services',
-          value: 'aviation-services',
-        },
-        {
-          text: 'Construction',
-          value: 'construction',
-        },
-        {
-          text: 'Consulting/Advisory services',
-          value: 'consulting',
-        },
-        {
-          text: 'Distribution/ Logistics & Shipping',
-          value: 'distribution-industry',
-        },
-        {
-          text: 'Energy efficiency contracting',
-          value: 'energy-efficiency-contracting',
-        },
-        {
-          text: 'Engineering',
-          value: 'engineering',
-        },
-        {
-          text: 'Engineering, Procurement, Construction',
-          value: 'epc',
-        },
-        {
-          text: 'Enviro. consulting, engineering, construction',
-          value: 'ecec',
-        },
-        {
-          text: 'Full facilities management service',
-          value: 'facilities-management-industry',
-        },
-        {
-          text: 'Grounds maintenance',
-          value: 'grounds-maintenance',
-        },
-        {
-          text: 'Janitorial services',
-          value: 'janitorial-services',
-        },
-        {
-          text: 'Freight & shipping',
-          value: 'freight-and-shipping',
-        },
-        {
-          text: 'Fuel supply',
-          value: 'fuel-supply',
-        },
-        {
-          text: 'General contracting',
-          value: 'general-contractors',
-        },
-        {
-          text: 'Interior design',
-          value: 'interior-design',
-        },
-        {
-          text: 'Lab services',
-          value: 'lab-services',
-        },
-        {
-          text: 'Boxes & enclosures',
-          value: 'boxes-and-enclosures',
-        },
-        {
-          text: 'Distribution transformers',
-          value: 'distribution-transformers',
-        },
-        {
-          text: 'Fabricated metals',
-          value: 'fabricated-metals',
-        },
-        {
-          text: 'General manufacturing',
-          value: 'general-manufacturing',
-        },
-        {
-          text: 'Wire & cable',
-          value: 'wire-and-cable',
-        },
-        {
-          text: 'Marketing & communications',
-          value: 'marketing-and-communications',
-        },
-        {
-          text: 'Print services',
-          value: 'print-services',
-        },
-        {
-          text: 'Professional services',
-          value: 'office-space-only',
-        },
-        {
-          text: 'Rental equipment & parts',
-          value: 'vehicle-equipment-and-parts',
-        },
-        {
-          text: 'Value added reselling',
-          value: 'shipping-and-logistics',
-        },
-        {
-          text: 'Vegetation management',
-          value: 'vegetation-industry',
-        },
-        {
-          text: 'Waste management',
-          value: 'waste-industry',
-        },
-      ])
+      resolve({
+        count: 5,
+        next: null,
+        previous: null,
+        results: [
+          {
+            path: '/consulting',
+            title: 'Consulting/Advisory services',
+            indent: 0,
+          },
+          { path: '/construction', title: 'Construction', indent: 0 },
+          { path: null, title: 'Metal structures & equipment', indent: 0 },
+          {
+            path: '/metal/boxes-and-enclosures',
+            title: 'Boxes & enclosures',
+            indent: 1,
+          },
+          { path: '/metal/wire-and-cable', title: 'Wire & cable', indent: 1 },
+          {
+            path: '/professional-services',
+            title: 'Professional services',
+            indent: 0,
+          },
+        ],
+      })
     }, DELAY)
   })
 }
@@ -128,16 +43,20 @@ export function getIndustrySegments() {
 export function getPreviousIndustrySegments() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve([
-        {
-          text: 'Construction',
-          value: 'construction',
-        },
-        {
-          text: 'Engineering, Procurement, Construction',
-          value: 'epc',
-        },
-      ])
+      resolve({
+        count: 3,
+        next: null,
+        previous: null,
+        results: [
+          { path: '/construction', title: 'Construction', indent: 0 },
+          { path: null, title: 'Metal structures & equipment', indent: 0 },
+          {
+            path: '/metal/boxes-and-enclosures',
+            title: 'Boxes & enclosures',
+            indent: 1,
+          },
+        ],
+      })
     }, DELAY)
   })
 }
