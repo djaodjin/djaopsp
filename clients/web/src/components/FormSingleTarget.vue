d7380c2a9deab6f7b5d79f7a2c66d73bb11fc350
David Quiros <david@quiros.online>
2020-07-30 23:32:24 -0600

    Refactor/create FormSingleTarget

---
+++ clients/web/src/components/FormSingleTarget.vue
@@ -0,0 +1,106 @@
+<template>
+  <fragment>
+    <v-checkbox class="mt-2" v-model="target.include" hide-details>
+      <template v-slot:label>
+        <b>{{ label }}</b>
+      </template>
+    </v-checkbox>
+
+    <v-expand-transition>
+      <div class="pl-8 py-1" v-show="target.include">
+        <v-menu
+          :close-on-content-click="false"
+          v-model="menuBy"
+          transition="scale-transition"
+          offset-y
+          max-width="290"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+              v-bind="attrs"
+              v-on="on"
+              hide-details="auto"
+              v-model="target.dateBy"
+              label="By"
+              append-icon="mdi-calendar"
+              readonly
+            ></v-text-field>
+          </template>
+          <v-date-picker
+            v-model="target.dateBy"
+            :min="new Date().toISOString()"
+            @input="menuBy = false"
+          ></v-date-picker>
+        </v-menu>
+
+        <v-textarea
+          class="mt-4"
+          :label="$t('targets.tab1.form.textarea-target')"
+          v-model="target.text"
+          hide-details="auto"
+          auto-grow
+          outlined
+          rows="4"
+          row-height="16"
+        ></v-textarea>
+
+        <v-menu
+          :close-on-content-click="false"
+          v-model="menuBaseline"
+          transition="scale-transition"
+          offset-y
+          max-width="290"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+              v-bind="attrs"
+              v-on="on"
+              hide-details="auto"
+              v-model="target.dateBaseline"
+              label="Baseline"
+              append-icon="mdi-calendar"
+              readonly
+            ></v-text-field>
+          </template>
+          <v-date-picker
+            v-model="target.dateBaseline"
+            :max="new Date().toISOString()"
+            @input="menuBaseline = false"
+          ></v-date-picker>
+        </v-menu>
+
+        <v-textarea
+          class="mt-4"
+          :label="$t('targets.tab1.form.textarea-comments')"
+          v-model="target.comments"
+          hide-details="auto"
+          auto-grow
+          outlined
+          rows="4"
+          row-height="16"
+        ></v-textarea>
+      </div>
+    </v-expand-transition>
+  </fragment>
+</template>
+
+<script>
+import { Fragment } from 'vue-fragment'
+
+export default {
+  name: 'FormSingleTarget',
+
+  props: ['label', 'target'],
+
+  data() {
+    return {
+      menuBy: false,
+      menuBaseline: false,
+    }
+  },
+
+  components: {
+    Fragment,
+  },
+}
+</script>
