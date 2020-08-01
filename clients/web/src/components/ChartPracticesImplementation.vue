a0febdf03a4a32b87bfbaa931be17e755fcb14b5
David Quiros <david@quiros.online>
2020-07-31 21:32:44 -0600

    Decrease chart y-margins for mobile

--- clients/web/src/components/ChartPracticesImplementation.vue
+++ clients/web/src/components/ChartPracticesImplementation.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mt-8 mb-6">
+  <div class="mt-6 mb-4 mt-md-8 mb-md-6">
     <practice-group-header class="mb-2" :section="section" />
     <svg
       class="chart-practices-implementation"
