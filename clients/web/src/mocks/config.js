89f7066f8fb6c611567d217c7ce201dd9c4e3033
David Quiros <david@quiros.online>
2020-08-06 16:43:05 -0600

    Refactor mock server init; add properties to assessment factory

--- clients/web/src/mocks/config.js
+++ clients/web/src/mocks/config.js
@@ -1,1 +1,32 @@
 export const DELAY = 100
+
+export const INDUSTRIES = [
+  {
+    name: 'Architectural Design',
+    path: 'sustainability-architecture-design',
+  },
+  {
+    name: 'Aviation Services',
+    path: 'sustainability-aviation-services',
+  },
+  {
+    name: 'Construction',
+    path: 'sustainability-construction',
+  },
+  {
+    name: 'Consulting/Advisory Services',
+    path: 'sustainability-consulting',
+  },
+  {
+    name: 'Distribution/Logistics & Shipping',
+    path: 'sustainability-distribution-industry',
+  },
+  {
+    name: 'Energy efficiency contracting',
+    path: 'sustainability-energy-efficiency-contracting',
+  },
+  {
+    name: 'Freight & Shipping',
+    path: 'sustainability-freight-and-shipping',
+  },
+]
--- clients/web/src/mocks/server.js
+++ clients/web/src/mocks/server.js
@@ -1,13 +1,18 @@
+import { INDUSTRIES } from './config'
 import {
-  STEP_PRACTICE_KEY,
-  STEP_TARGETS_KEY,
-  STEP_PLAN_KEY,
-  STEP_FREEZE_KEY,
+  DEFAULT_ASSESSMENT_STEP,
   STEP_SCORECARD_KEY,
-  STEP_SHARE_KEY,
+  VALID_ASSESSMENT_STEPS,
   VALID_ASSESSMENT_TARGETS_KEYS,
 } from '@/config/app'
-import { RestSerializer, Server, Model, Factory, hasMany } from 'miragejs'
+import {
+  RestSerializer,
+  Server,
+  Model,
+  Factory,
+  hasMany,
+  belongsTo,
+} from 'miragejs'
 import faker from 'faker'
 
 const ApplicationSerializer = RestSerializer.extend()
@@ -20,18 +25,37 @@ export function makeServer({ environment = 'development' }) {
     environment,
 
     models: {
+      answer: Model.extend({
+        question: belongsTo(),
+      }),
       assessment: Model.extend({
         targets: hasMany(),
+        improvementPlan: hasMany('practices'),
       }),
       organization: Model.extend({
         assessments: hasMany(),
+      }),
+      practice: Model.extend({
+        question: belongsTo(),
+      }),
+      question: Model.extend({
+        previousAnswers: hasMany('answers'),
       }),
       target: Model,
     },
 
     factories: {
       assessment: Factory.extend({
-        improvementPlan: [],
+        industryPath() {
+          return faker.random.arrayElement(INDUSTRIES).path
+        },
+        industryName() {
+          const industry = INDUSTRIES.find((i) => i.path === this.industryPath)
+          return industry.name
+        },
+        status() {
+          return DEFAULT_ASSESSMENT_STEP
+        },
       }),
       target: Factory.extend({
         text() {
@@ -51,18 +75,16 @@ export function makeServer({ environment = 'development' }) {
     },
 
     seeds(server) {
-      // Organization with active assessment with environmental targets
+      // Organization with active assessment with existing environmental targets and improvement plan
       server.create('organization', {
         id: 'marlin',
         name: 'Blue Marlin',
         assessments: [
           server.create('assessment', {
-            industryName: 'Freight & Shipping',
-            industryPath: 'sustainability-freight-and-shipping',
             targets: VALID_ASSESSMENT_TARGETS_KEYS.map((key) =>
               server.create('target', { key })
             ),
-            status: STEP_PLAN_KEY,
+            status: STEP_SCORECARD_KEY,
           }),
         ],
       })
@@ -71,38 +93,11 @@ export function makeServer({ environment = 'development' }) {
       server.create('organization', {
         id: 'steve-shop',
         name: 'Steve Test Shop',
-        assessments: [
+        assessments: VALID_ASSESSMENT_STEPS.map((step) =>
           server.create('assessment', {
-            industryName: 'Architectural Design',
-            industryPath: 'sustainability-architecture-design',
-            status: STEP_PRACTICE_KEY,
-          }),
-          server.create('assessment', {
-            industryName: 'Aviation Services',
-            industryPath: 'sustainability-aviation-services',
-            status: STEP_TARGETS_KEY,
-          }),
-          server.create('assessment', {
-            industryName: 'Construction',
-            industryPath: 'sustainability-construction',
-            status: STEP_PLAN_KEY,
-          }),
-          server.create('assessment', {
-            industryName: 'Consulting/Advisory Services',
-            industryPath: 'sustainability-consulting',
-            status: STEP_SCORECARD_KEY,
-          }),
-          server.create('assessment', {
-            industryName: 'Distribution/Logistics & Shipping',
-            industryPath: 'sustainability-distribution-industry',
-            status: STEP_FREEZE_KEY,
-          }),
-          server.create('assessment', {
-            industryName: 'Energy efficiency contracting',
-            industryPath: 'sustainability-energy-efficiency-contracting',
-            status: STEP_SHARE_KEY,
-          }),
-        ],
+            status: step,
+          })
+        ),
       })
 
       // Organization with no assessments
