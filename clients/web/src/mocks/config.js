bf5cbfd0c909b5fe3acdeef7057c21c76f02ba7e
David Quiros <david@quiros.online>
2020-08-26 22:18:11 -0600

    Remove unused constant INDUSTRIES

--- clients/web/src/mocks/config.js
+++ clients/web/src/mocks/config.js
@@ -2,38 +2,6 @@ export const NUM_BENCHMARKS = 6
 
 export const BENCHMARK_MAX_COMPANIES = 30
 
-// TODO: Remove this if there is no relationship between industry and assessment
-export const INDUSTRIES = [
-  {
-    name: 'Architectural Design',
-    path: 'sustainability-architecture-design',
-  },
-  {
-    name: 'Aviation Services',
-    path: 'sustainability-aviation-services',
-  },
-  {
-    name: 'Construction',
-    path: 'sustainability-construction',
-  },
-  {
-    name: 'Consulting/Advisory Services',
-    path: 'sustainability-consulting',
-  },
-  {
-    name: 'Distribution/Logistics & Shipping',
-    path: 'sustainability-distribution-industry',
-  },
-  {
-    name: 'Energy efficiency contracting',
-    path: 'sustainability-energy-efficiency-contracting',
-  },
-  {
-    name: 'Freight & Shipping',
-    path: 'sustainability-freight-and-shipping',
-  },
-]
-
 export const INDUSTRY_SECTIONS = [
   {
     name: 'Governance & Management',
