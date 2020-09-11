f2e1a3f9ea99b2a65bf0d1978756961adfcfd319
David Quiros <david@quiros.online>
2020-09-11 12:34:00 -0600

    Enable answer formatting using HTML

--- clients/web/src/components/QuestionPreviousAnswers.vue
+++ clients/web/src/components/QuestionPreviousAnswers.vue
@@ -1,7 +1,7 @@
 <template>
   <v-card outlined class="d-inline-block py-3 px-4 mb-6" v-if="previousAnswer">
     <span class="mr-1">Previous Answer:</span>
-    <b>{{ answerText }}</b>
+    <b v-html="answerText" />
     <p class="text-caption mt-1 mb-0">
       Submitted by {{ previousAnswer.author }} on
       <span v-format-date>{{ previousAnswer.created }}</span> in previous
