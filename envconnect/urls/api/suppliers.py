b8991f4ef84b97f61e9957251e75da8562f1ee50
Sebastien Mirolo <smirolo@djaodjin.com>
2020-09-23 17:41:17 -0700

    adds eol marker so api doc is generated correctly

--- envconnect/urls/api/suppliers.py
+++ envconnect/urls/api/suppliers.py
@@ -14,7 +14,7 @@ from ...api.improvements import (ImprovementListAPIView,
     ImprovementAnswerAPIView)
 
 urlpatterns = [
-    url(r'(?P<organization>%s)/suppliers/(?P<path>%s)'
+    url(r'(?P<organization>%s)/suppliers/(?P<path>%s)$'
         % (SLUG_RE, PATH_RE),
         SupplierListAPIView.as_view(), name="api_suppliers"),
     url(r'(?P<organization>%s)/matrix/(?P<path>%s)' % (
@@ -25,36 +25,36 @@ urlpatterns = [
     url(r'(?P<organization>%s)/' % SLUG_RE,
         include('survey.urls.api.matrix')),
 
-    url(r'(?P<organization>%s)/benchmark/share/(?P<path>%s)' % (
+    url(r'(?P<organization>%s)/benchmark/share/(?P<path>%s)$' % (
         SLUG_RE, PATH_RE),
         ShareScorecardAPIView.as_view(),
         name="api_scorecard_share"),
-    url(r'(?P<organization>%s)/benchmark/historical/(?P<path>%s)' % (
+    url(r'(?P<organization>%s)/benchmark/historical/(?P<path>%s)$' % (
         SLUG_RE, PATH_RE),
         HistoricalScoreAPIView.as_view(),
         name="api_historical_scores"),
     url(r'(?P<organization>%s)/benchmark/(?P<sample>%s)/'\
-        'details/(?P<path>%s)' % (SLUG_RE, SLUG_RE, PATH_RE),
+        'details/(?P<path>%s)$' % (SLUG_RE, SLUG_RE, PATH_RE),
         PracticeBenchmarkAPIView.as_view(),
         name="api_benchmark_practices"),
     url(r'(?P<organization>%s)/benchmark/(?P<sample>%s)/'\
-        'graphs/(?P<path>%s)' % (SLUG_RE, SLUG_RE, PATH_RE),
+        'graphs/(?P<path>%s)$' % (SLUG_RE, SLUG_RE, PATH_RE),
         BenchmarkAPIView.as_view(),
         name="api_benchmark"),
     url(r'(?P<organization>%s)/improvement$' % SLUG_RE,
         ImprovementListAPIView.as_view(),
         name='api_improvement_base'),
-    url(r'(?P<organization>%s)/improvement/(?P<path>%s)' % (
+    url(r'(?P<organization>%s)/improvement/(?P<path>%s)$' % (
         SLUG_RE, PATH_RE),
         ImprovementAnswerAPIView.as_view(),
         name='api_improvement'),
-    url(r'(?P<organization>%s)/sample/(?P<sample>%s)/answers/(?P<path>%s)' % (
+    url(r'(?P<organization>%s)/sample/(?P<sample>%s)/answers/(?P<path>%s)$' % (
         SLUG_RE, SLUG_RE, PATH_RE),
         AssessmentAnswersAPIView.as_view(), name='survey_api_sample_answers'),
-    url(r'(?P<organization>%s)/sample/(?P<sample>%s)/reset/(?P<path>%s)' % (
+    url(r'(?P<organization>%s)/sample/(?P<sample>%s)/reset/(?P<path>%s)$' % (
         SLUG_RE, SLUG_RE, PATH_RE),
         AssessmentResetAPIView.as_view(), name='survey_api_sample_reset'),
-    url(r'(?P<organization>%s)/sample/(?P<sample>%s)/freeze/(?P<path>%s)' % (
+    url(r'(?P<organization>%s)/sample/(?P<sample>%s)/freeze/(?P<path>%s)$' % (
         SLUG_RE, SLUG_RE, PATH_RE),
         AssessmentFreezeAPIView.as_view(), name='survey_api_sample_freeze'),
     url(r'(?P<organization>%s)/sample/(?P<sample>%s)' % (
