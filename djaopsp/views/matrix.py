djaodjin/djaopsp

commit 77025a22dd7f3fea51ed3b7762383298c157956b

moves get_scored_assessments into queries.py

djaopsp/views/matrix.py
@@ -5,6 +5,7 @@ import datetime, io, logging
 
 from deployutils.helpers import datetime_or_now, update_context_urls
 from django.db import connection
+from django.db.models.query import QuerySet, RawQuerySet
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
 from openpyxl import Workbook
@@ -225,7 +226,22 @@ class CompareXLSXView(SupplierListMixin, TemplateView):
         #pylint:disable=too-many-statements,too-many-locals
         self.by_paths = {}
         accounts_with_response = set([])
-        if self.requested_accounts_pk_as_sql:
+        accounts = self.requested_accounts
+        sep = ""
+        accounts_clause = ""
+        if accounts:
+            if isinstance(accounts, list):
+                account_ids = "(%s)" % ','.join([
+                    str(account_id) for account_id in accounts])
+            elif isinstance(accounts, QuerySet):
+                account_ids = "(%s)" % ','.join([
+                    str(account.pk) for account in accounts])
+            elif isinstance(accounts, RawQuerySet):
+                account_ids = "(%s)" % accounts.query.sql
+            accounts_clause += (
+                "%(sep)ssamples.account_id IN %(account_ids)s" % {
+                    'sep': sep, 'account_ids': account_ids})
+        if accounts_clause:
             reporting_answers_sql = """
 WITH samples AS (
     %(latest_assessments)s
@@ -244,7 +260,7 @@ INNER JOIN samples
   ON survey_answer.sample_id = samples.id
 INNER JOIN survey_unit
   ON survey_answer.unit_id = survey_unit.id
-WHERE samples.account_id IN %(account_ids)s)
+WHERE %(accounts_clause)s
 SELECT
   answers.path,
   answers.account_id,
@@ -259,7 +275,7 @@ LEFT OUTER JOIN survey_choice
      survey_choice.id = answers.measured
 """ % {
             'latest_assessments': self.latest_assessments.query.sql,
-            'account_ids': self.requested_accounts_pk_as_sql
+            'accounts_clause': accounts_clause
         }
             comments_unit = Unit.objects.get(slug='freetext')
             with connection.cursor() as cursor:

djaopsp/api/portfolios.py
@@ -33,12 +33,13 @@ from .campaigns import CampaignContentMixin
 from ..compat import reverse, six
 from ..helpers import as_percentage
 from ..queries import (get_completed_assessments_at_by, get_engagement,
-    get_engagement_by_reporting_status, get_requested_by_accounts_by_year)
+    get_engagement_by_reporting_status, get_requested_by_accounts_by_year,
+    segments_as_sql)
 from ..mixins import (AccountMixin, AccountsAggregatedQuerysetMixin,
     AccountsNominativeQuerysetMixin, CampaignMixin)
 from ..models import ScorecardCache
 from ..utils import (TransparentCut, get_alliances, get_latest_reminders,
-    get_segments_candidates, segments_as_sql)
+    get_segments_candidates)
 from .rollups import GraphMixin, RollupMixin, ScoresMixin
 from .serializers import (AccessiblesSerializer, CompareNodeSerializer,
     EngagementSerializer, ReportingSerializer)
@@ -299,15 +300,6 @@ class SupplierListMixin(ScoresMixin, AccountsNominativeQuerysetMixin):
       - user.last_name
       - created_at
     """
-    @property
-    def requested_accounts_pk_as_sql(self):
-        if not hasattr(self, '_requested_accounts_pk_as_sql'):
-            self._requested_accounts_pk_as_sql = ""
-            pks = [str(account.pk) for account in self.requested_accounts]
-            if pks:
-                self._requested_accounts_pk_as_sql = "(%s)" % ','.join(pks)
-        return self._requested_accounts_pk_as_sql
-
     def decorate_queryset(self, queryset):
         """
         Updates `normalized_score` in rows of the queryset.

djaopsp/api/samples.py
@@ -13,7 +13,7 @@ from rest_framework import generics, response as http, status as http_status
 from rest_framework.exceptions import ValidationError
 from survey.api.sample import (SampleCandidatesMixin, SampleAnswersMixin,
     SampleFreezeAPIView)
-from survey.api.matrix import SampleBenchmarksAPIView
+from survey.api.matrix import SampleBenchmarksAPIView, SampleBenchmarkMixin
 from survey.mixins import TimersMixin
 from survey.models import Choice, Sample, Unit, UnitEquivalences
 from survey.settings import DB_PATH_SEP
@@ -23,11 +23,12 @@ from ..compat import reverse, six
 from ..mixins import SectionReportMixin
 from ..models import ScorecardCache
 from ..scores import freeze_scores, get_score_calculator
+from ..queries import get_scored_assessments
 from ..utils import get_practice_serializer, get_scores_tree, get_score_weight
 from .campaigns import CampaignContentMixin
 from .rollups import GraphMixin, RollupMixin, ScoresMixin
 from .serializers import (AssessmentNodeSerializer,
-    BenchmarkSerializer, UnitSerializer)
+    SampleBenchmarksSerializer, UnitSerializer)
 
 
 LOGGER = logging.getLogger(__name__)
@@ -701,8 +702,10 @@ class AssessmentContentIndexAPIView(AssessmentContentAPIView):
     """
 
 
-class BenchmarkAPIView(SectionReportMixin, GraphMixin, RollupMixin,
-                       ScoresMixin, SampleBenchmarksAPIView):
+class BenchmarkAPIView(TimersMixin, GraphMixin, RollupMixin,
+                       SampleBenchmarkMixin,
+                       SectionReportMixin, CampaignContentMixin,
+                       generics.ListAPIView):
     """
     Retrieves benchmark graphs
 
@@ -775,22 +778,9 @@ class BenchmarkAPIView(SectionReportMixin, GraphMixin, RollupMixin,
             "score_weight": 1.0
          }]
     """
-    serializer_class = BenchmarkSerializer
     pagination_class = None
-    DB_PATH_SEP = '/'
     account_model = get_account_model()
-
-    @property
-    def requested_accounts_pk_as_sql(self):
-        """
-        Returns all accounts available as a string
-        that can be used in SQL statements.
-        """
-        #pylint:disable=attribute-defined-outside-init
-        if not hasattr(self, '_requested_accounts_pk_as_sql'):
-            self._requested_accounts_pk_as_sql = (
-                "(SELECT id FROM %s)" % self.account_model._meta.db_table)
-        return self._requested_accounts_pk_as_sql
+    serializer_class = SampleBenchmarksSerializer
 
     @property
     def scores_tree(self):
@@ -814,12 +804,29 @@ class BenchmarkAPIView(SectionReportMixin, GraphMixin, RollupMixin,
             self._scores_of_interest = flatten_content_tree(self.scores_tree)
         return self._scores_of_interest
 
-
     def list(self, request, *args, **kwargs):
-        #pylint:disable=unused-argument,too-many-nested-blocks
         self._start_time()
-        queryset = self.get_queryset()
-        for score in queryset:
+        queryset = self.filter_queryset(self.get_queryset())
+        self.decorate_queryset(queryset)
+
+        # Ready to serialize
+        serializer = self.get_serializer_class()(
+            queryset, many=True, context=self.get_serializer_context())
+
+        self._report_queries("BenchmarkAPIView.list done")
+        return http.Response(serializer.data)
+
+
+    def decorate_queryset(self, queryset):
+        #pylint:disable=unused-argument,too-many-nested-blocks
+        # `queryset` is a list of questions
+
+        accounts = None # XXX self.get_accessible_accounts()
+        scored_assessments = get_scored_assessments(
+            self.campaign, accounts=accounts,
+            scores_of_interest=self.scores_of_interest, db_path=self.db_path,
+            ends_at=self.ends_at)
+        for score in scored_assessments:
             if score.segment_path:
                 self._insert_in_tree(
                     self.scores_tree, score.segment_path, score)
@@ -847,7 +854,27 @@ class BenchmarkAPIView(SectionReportMixin, GraphMixin, RollupMixin,
                                 break
         self._report_queries("sample scorecard_cache computed")
 
-        return http.Response(charts)
+        for question in queryset:
+            question_path = question.get('path')
+            benchmarks = question.get('benchmarks')
+            default_unit = question.get('default_unit')
+            if default_unit:
+                question.update({
+                    'default_unit': Unit.objects.get(slug=default_unit),
+                })
+            for chart in charts:
+                chart_path = chart.get('path')
+                if question_path == chart_path:
+                    question.update({
+                        'organization_rate': chart.get('organization_rate'),
+                        'benchmarks': chart.get('benchmarks'),
+                        'nb_respondents': chart.get('nb_respondents'),
+                        'avg_normalized_score':
+                            chart.get('avg_normalized_score'),
+                        'highest_normalized_score':
+                            chart.get('highest_normalized_score')
+                    })
+                    break
 
 
 class BenchmarkIndexAPIView(BenchmarkAPIView):

djaopsp/api/serializers.py
@@ -22,32 +22,6 @@ class NoModelSerializer(serializers.Serializer):
         raise RuntimeError('`update()` should not be called.')
 
 
-class DistributionSerializer(NoModelSerializer):
-
-    x = serializers.ListField(serializers.CharField())
-    y = serializers.ListField(child=serializers.IntegerField())
-    organization_rate = serializers.CharField()
-
-
-class BenchmarkSerializer(NoModelSerializer):
-
-    slug = serializers.CharField()
-    title = serializers.CharField()
-    nb_answers = serializers.IntegerField(required=False)
-    nb_questions = serializers.IntegerField(required=False)
-    nb_respondents = serializers.IntegerField(required=False)
-    numerator = serializers.FloatField(required=False)
-    improvement_numerator = serializers.FloatField(required=False)
-    denominator = serializers.FloatField(required=False)
-    normalized_score = serializers.IntegerField(required=False)
-    improvement_score = serializers.IntegerField(required=False)
-    score_weight = serializers.FloatField()
-    highest_normalized_score = serializers.IntegerField(required=False)
-    avg_normalized_score = serializers.IntegerField(required=False)
-    created_at = serializers.DateTimeField(required=False)
-    distribution = DistributionSerializer(required=False)
-
-
 class PracticeSerializer(BaseNodeElementSerializer):
 
     default_unit = serializers.SerializerMethodField()
@@ -170,7 +144,7 @@ class AssessmentNodeSerializer(ContentNodeSerializer):
     # assessment results
     normalized_score = serializers.SerializerMethodField(required=False)
     nb_respondents = serializers.SerializerMethodField(required=False)
-    rate = serializers.SerializerMethodField(required=False)
+    rate = serializers.SerializerMethodField(required=False) # XXX to deprecate
     opportunity = serializers.SerializerMethodField(required=False)
 
     class Meta(ContentNodeSerializer.Meta):
@@ -245,6 +219,24 @@ class AssessmentNodeSerializer(ContentNodeSerializer):
         except (TypeError, KeyError):
             pass
         return None
+
+
+class SampleBenchmarksSerializer(ContentNodeSerializer):
+
+    avg_normalized_score = serializers.IntegerField(required=False)
+    highest_normalized_score = serializers.IntegerField(required=False)
+    nb_respondents = serializers.IntegerField(required=False)
+    benchmarks = serializers.ListField(child=TableSerializer(), required=False)
+    organization_rate = serializers.CharField(required=False) # XXX should move
+    # to AssessmentNodeSerializer.
+
+    class Meta(ContentNodeSerializer.Meta):
+        fields = ContentNodeSerializer.Meta.fields + (
+            'avg_normalized_score', 'highest_normalized_score',
+            'nb_respondents', 'organization_rate', 'benchmarks',)
+        read_only_fields = ContentNodeSerializer.Meta.read_only_fields + (
+            'avg_normalized_score', 'highest_normalized_score',
+            'nb_respondents', 'organization_rate', 'benchmarks',)
 
 
 class CompareNodeSerializer(PRACTICE_SERIALIZER):

djaopsp/jinja2.py
@@ -26,7 +26,9 @@ def environment(**options):
             loader_class = options['loader'].__class__
         options['loader'] = loader_class(get_template_search_path())
     env = Jinja2Environment(extensions=[i18n], **options)
-    # i18n
+    # `jinja2.ext.i18n` was added to the extensions so it is safe to disable
+    # pylint warning here.
+    #pylint:disable=no-member
     env.install_gettext_callables(gettext=gettext, ngettext=ngettext,
         newstyle=True)
     # Generic filters to render pages

djaopsp/mixins.py
@@ -507,6 +507,12 @@ class AccountsAggregatedQuerysetMixin(DateRangeContextMixin, AccountMixin):
                     self._accounts_start_at = accounts_ends_at
         return self._accounts_start_at
 
+    @property
+    def search_terms(self):
+        if not hasattr(self, '_search_terms'):
+            self._search_terms = self.get_query_param('q', None)
+        return self._search_terms
+
     def get_requested_accounts(self, grantee, aggregate_set=False):
         """
         All accounts which ``grantee`` has requested a scorecard from.
@@ -515,7 +521,8 @@ class AccountsAggregatedQuerysetMixin(DateRangeContextMixin, AccountMixin):
             grantee = self.account
         return get_requested_accounts(grantee,
             campaign=self.campaign, aggregate_set=aggregate_set,
-            start_at=self.accounts_start_at, ends_at=self.accounts_ends_at)
+            start_at=self.accounts_start_at, ends_at=self.accounts_ends_at,
+            search_terms=self.search_terms)
 
 
 class AccountsNominativeQuerysetMixin(AccountsAggregatedQuerysetMixin):

commit 9c73c674508ed2079120a28b4b685efc8206d64f

fixes typo introduced in commit 77025a22

djaopsp/views/matrix.py
@@ -261,7 +261,7 @@ INNER JOIN samples
   ON survey_answer.sample_id = samples.id
 INNER JOIN survey_unit
   ON survey_answer.unit_id = survey_unit.id
-WHERE %(accounts_clause)s
+WHERE %(accounts_clause)s)
 SELECT
   answers.path,
   answers.account_id,