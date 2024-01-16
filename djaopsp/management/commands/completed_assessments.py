d735dd55eb69755abbbfe3cfcda5fdb1c3163551
Noman Bukhari <nbukhari96@gmail.com>
2024-01-16 00:44:53 -0800

    Updated logic to allow for a grantee
    - Preliminary version of updated logic in the completed_assessments command to allow specifying a grantee.

--- djaopsp/management/commands/completed_assessments.py
+++ djaopsp/management/commands/completed_assessments.py
@@ -9,10 +9,11 @@ import logging
 from dateutil.relativedelta import relativedelta
 from django.core.management.base import BaseCommand
 from django.urls import reverse
-from survey.models import Sample, Campaign
+from survey.models import Campaign
 from survey.queries import datetime_or_now
 
 from ...models import ScorecardCache
+from ...queries import get_portfolios_frozen_assessments
 
 LOGGER = logging.getLogger(__name__)
 
@@ -21,10 +22,13 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         super(Command, self).add_arguments(parser)
-        parser.add_argument('campaign',
+        parser.add_argument('--campaign',
                             action='store',
                             help='Slug of the campaign',
                             default=None)
+        parser.add_argument('--grantee',
+                            action='store',
+                            help='Slug of grantee')
         parser.add_argument('--starts_at',
                             action='store',
                             help='Start date, in YYYY-MM-DD format')
@@ -33,21 +37,40 @@ class Command(BaseCommand):
                             help='End date, in YYYY-MM-DD format')
 
     def handle(self, *args, **options):
-        campaign = options['campaign']
+        campaign_slug = options['campaign']
+        grantee = options['grantee']
         ends_at = datetime_or_now(options['ends_at'])
         starts_at = datetime_or_now(options['starts_at']) if options['starts_at'] \
                 else ends_at - relativedelta(weeks=1)
+        
+        campaign = None
+        if campaign_slug:
+            try:
+                campaign = Campaign.objects.get(slug=campaign_slug)
+            except Campaign.DoesNotExist:
+                self.stdout.write(f"No campaign found with slug '{campaign_slug}'.")
+                return
+
+        filter_args = {}
         if campaign:
-            campaign = Campaign.objects.get(slug=str(campaign))
+            filter_args['campaign'] = campaign
+        if grantee:
+            filter_args['grantee'] = grantee
 
-        # We're returning completed/frozen samples belonging to a campaign
-        completed_surveys_raw = Sample.objects.get_latest_frozen_by_accounts(
-            campaign=campaign, start_at=starts_at, ends_at=ends_at
-        )
-        completed_surveys = Sample.objects.filter(
-            pk__in=[sample.pk for sample in completed_surveys_raw])
+        if not campaign and not grantee:
+            raise ValueError("Either 'campaign' or 'grantee' must be provided")
 
-        self.stdout.write(f"Completed assessments for {campaign.title}:")
+        _, completed_surveys, campaigns = get_portfolios_frozen_assessments(
+             **filter_args)
+
+        if campaigns:
+            self.stdout.write(f"Completed assessments for the following campaign(s):")
+            for campaign_slug in campaigns:
+                campaign = Campaign.objects.get(slug=campaign_slug)
+                self.stdout.write(f"{campaign.title} \n")
+        elif campaign:
+            self.stdout.write(f"Completed assessments for {campaign.title}:")
+
         self.stdout.write(
             f"There were a total of {len(completed_surveys)} surveys completed "
             f"between {starts_at.strftime('%Y-%m-%d')} and {ends_at.strftime('%Y-%m-%d')}")
@@ -60,6 +83,7 @@ class Command(BaseCommand):
                         normalized_score = scorecard_cache.first().normalized_score
             self.stdout.write(
                 f"{i+1}. Supplier: {survey.account.slug}, \n"
+                f"Campaign: {survey.campaign}, \n"
                 f"Completed At: {survey.created_at}, \n"
                 f"score_url: {score_url}, \n"
                 f"Normalized Score: {normalized_score}, \n")
