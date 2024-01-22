a3c586919cba10ed7a57658ca7c583962d4b87a1
Sebastien Mirolo <smirolo@djaodjin.com>
2024-01-22 13:30:37 -0800

    fixes import error for CertificateDownloadView

--- djaopsp/urls/views/content.py
+++ djaopsp/urls/views/content.py
@@ -5,13 +5,13 @@
 Views URLs
 """
 from django.urls import path, re_path
-from ...views.content import (ContentDetailView, ContentIndexView, 
+from ...views.content import (ContentDetailView, ContentIndexView,
      SequenceProgressView, SequencePageElementView)
-from pages.views.elements import CertificateDownloadView
+from pages.views.sequences import CertificateDownloadView
 
 
 urlpatterns = [
-    path('sequences/<slug:sequence>/', 
+    path('sequences/<slug:sequence>/',
          SequenceProgressView.as_view(), name='sequence_progress_view'),
     re_path(r'sequences/(?P<sequence>[^/]+)/(?P<rank>-?\d+)/',
           SequencePageElementView.as_view(),
