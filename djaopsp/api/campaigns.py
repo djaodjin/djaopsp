djaodjin/djaopsp

commit 8d8ef456be73cbb51b05cd783cfe40cafdf4150f

updates to campaign editor

djaopsp/api/campaigns.py
@@ -22,7 +22,8 @@ from survey.filters import SearchFilter
 from survey.helpers import extra_as_internal
 from survey.mixins import QuestionMixin
 from survey.models import EnumeratedQuestions, Unit
-from survey.utils import get_question_model, get_question_serializer
+from survey.utils import (get_content_model, get_question_model,
+    get_question_serializer)
 from survey.settings import DB_PATH_SEP
 
 from .serializers import ContentNodeSerializer, CreateContentElementSerializer
@@ -370,6 +371,7 @@ class CampaignEditableContentAPIView(CampaignContentMixin,
 
     serializer_class = ContentNodeSerializer
     strip_segment_prefix = True
+    default_unit = Unit.objects.get(slug='freetext')
 
     def get_serializer_class(self):
         if self.request.method.lower() == 'post':
@@ -531,7 +533,7 @@ class CampaignEditableContentAPIView(CampaignContentMixin,
                     part.slug for part in prefix_parts] + [element.slug])
                 question = get_question_model().objects.create(
                     path=path, content=element,
-                    default_unit=Unit.objects.get(slug='freetext'))
+                    default_unit=self.default_unit)
                 EnumeratedQuestions.objects.get_or_create(
                     campaign=campaign, question=question, rank=rank)
 
@@ -653,6 +655,7 @@ class CampaignEditableQuestionAPIView(QuestionMixin, CampaignContentMixin,
         }
     """
     serializer_class = get_question_serializer()
+    default_unit = Unit.objects.get(slug='freetext')
 
     def get_serializer_class(self):
         if self.request.method.lower() in ('post',):
@@ -679,9 +682,8 @@ class CampaignEditableQuestionAPIView(QuestionMixin, CampaignContentMixin,
 
             DELETE /api/editables/alliance/campaigns/sustainability/construction HTTP/1.1
         """
-        #pylint:disable=useless-super-delegation
-        return super(CampaignEditableQuestionAPIView, self).delete(
-            request, *args, **kwargs)
+        return self.destroy(request, *args, **kwargs)
+
 
     def post(self, request, *args, **kwargs):
         """
@@ -713,9 +715,7 @@ class CampaignEditableQuestionAPIView(QuestionMixin, CampaignContentMixin,
                 "default_unit": "assessment"
             }
         """
-        #pylint:disable=useless-super-delegation
-        return super(CampaignEditableQuestionAPIView, self).post(
-            request, *args, **kwargs)
+        return self.create(request, *args, **kwargs)
 
     def put(self, request, *args, **kwargs):
         """
@@ -750,9 +750,79 @@ class CampaignEditableQuestionAPIView(QuestionMixin, CampaignContentMixin,
                 "default_unit": "assessment"
             }
         """
-        #pylint:disable=useless-super-delegation
-        return super(CampaignEditableQuestionAPIView, self).put(
-            request, *args, **kwargs)
+        return self.update(request, *args, **kwargs)
+
+
+    def perform_create(self, serializer):
+        content_model = get_content_model()
+        enum_question_model = EnumeratedQuestions
+        question_model = get_question_model()
+
+        with transaction.atomic():
+            content_data = {}
+            content_data.update(serializer.validated_data)
+            invalid_fields = []
+            model_fields = {
+                field.name for field in content_model._meta.get_fields()}
+            for field_name in six.iterkeys(content_data):
+                if field_name not in model_fields:
+                    invalid_fields += [field_name]
+            for field_name in invalid_fields:
+                content_data.pop(field_name)
+            title = content_data.get('title')
+            element, _ = content_model.objects.get_or_create(
+                title=title, account=self.campaign.account,
+                defaults=content_data)
+            # Attach the element in the content DAG
+            parent = self.element
+            rank = RelationShip.objects.filter(
+                orig_element=parent).aggregate(Max('rank')).get(
+                'rank__max', None)
+            rank = 0 if rank is None else rank + 1
+            RelationShip.objects.get_or_create(
+                orig_element=parent, dest_element=element,
+                defaults={'rank': rank})
+
+            question_data = {}
+            question_data.update(serializer.validated_data)
+            invalid_fields = []
+            model_fields = {
+                field.name for field in question_model._meta.get_fields()}
+            for field_name in six.iterkeys(question_data):
+                if field_name not in model_fields:
+                    invalid_fields += [field_name]
+            for field_name in invalid_fields:
+                question_data.pop(field_name)
+
+            question_data.update({'content': element})
+            if 'default_unit' not in question_data:
+                question_data.update({'default_unit': self.default_unit})
+            path = DB_PATH_SEP.join([self.db_path, element.slug])
+            question, _ = question_model.objects.update_or_create(
+                    path=path, defaults=question_data)
+
+            enum_question_data = {}
+            enum_question_data.update(serializer.validated_data)
+            invalid_fields = []
+            model_fields = {
+                field.name for field in enum_question_model._meta.get_fields()}
+            for field_name in six.iterkeys(enum_question_data):
+                if field_name not in model_fields:
+                    invalid_fields += [field_name]
+            for field_name in invalid_fields:
+                enum_question_data.pop(field_name)
+
+            if 'rank' not in enum_question_data:
+                rank = enum_question_model.objects.filter(
+                    campaign=self.campaign).aggregate(
+                        Max('rank')).get('rank__max', None)
+                enum_question_data.update({
+                    'rank': 0 if rank is None else rank + 1})
+            enum_question_model.objects.update_or_create(
+                campaign=self.campaign,
+                question=question,
+                defaults=enum_question_data)
+
 
     def update(self, request, *args, **kwargs):
         # Implementation note: overrides overide in `PageElementEditableDetail`

djaopsp/campaigns.py
@@ -87,6 +87,7 @@ def _import_campaign_section(campaign, rows, seg_prefixes,
     #pylint:disable=too-many-arguments,too-many-locals,too-many-nested-blocks
     if not content_model:
         content_model = get_content_model()
+    question_model = get_question_model()
 
     LOGGER.debug("%d segment prefixes: %s", len(seg_prefixes), seg_prefixes)
     freetext_unit = Unit.objects.get(slug='freetext')
@@ -178,7 +179,7 @@ def _import_campaign_section(campaign, rows, seg_prefixes,
                             question_defaults.update({'ui_hint': "textarea"})
                         LOGGER.debug("create question %s", path)
                         question, _ = \
-                            get_question_model().objects.get_or_create(
+                            question_model.objects.get_or_create(
                                 path=path, defaults=question_defaults)
                         EnumeratedQuestions.objects.get_or_create(
                             campaign=campaign,

djaopsp/static/js/editors-vue.js
@@ -22,15 +22,15 @@ Vue.component('editable-practices-list', {
             var vm = this;
             var parent = '';
             if( typeof entry !== 'undefined' ) {
-                parent = entry.slug;
+                parent = entry.path;
                 // We differentiate practices from headings by the presence and
-                // abscence, respectively, of a `url` field.
+                // abscence, respectively, of a `default_unit` field.
                 // When adding an element below an existing practice, we attach
                 // it to the preceding heading.
-                if( entry.url ) {
+                if( entry.default_unit ) {
                     var parentIndex = vm._findParentHeadingIndex(entry, index);
                     if( parentIndex > 0 ) {
-                        parent = vm.items.results[parentIndex].slug;
+                        parent = vm.items.results[parentIndex].path;
                     }
                 }
             }

djaopsp/templates/pages/editables/element.html
@@ -1,0 +1,1 @@
+{% extends "pages/element.html" %}

djaopsp/templates/survey/campaigns/campaign.html
@@ -142,71 +142,70 @@
                             <td>
                               <div :class="indentHeader(entry)">
                                 <div v-if="index == activeIndex">
-                                <div class="input-group">
-                                  <input class="form-control" type="text"
-                                         v-model="entry.title" />
-                                  <div class="input-group-btn">
-                                    <button class="btn btn-primary" type="submit" value="submit" @click="updateElement(entry, index)">Update</button>
+                                  <div class="input-group">
+                                    <input class="form-control" type="text"
+                                           v-model="entry.title" />
+                                    <div class="input-group-btn">
+                                      <button class="btn btn-primary" type="submit" value="submit" @click="updateElement(entry, index)">Update</button>
+                                    </div>
                                   </div>
-                                </div>
-
-                              <default-unit-typeahead inline-template
+                                  <default-unit-typeahead inline-template
                                           class="mt-2"
                                           :question="entry"
                                           ref="default_unit"
                                           v-on:selectitem="updateItemSelected"
                                           v-if="isPractice(entry)">
-                                <div>
-                                  <div class="input-group mb-3">
-                                    <!-- optional indicators -->
-                                    <span class="input-group-text">
-                                      <i class="fa fa-spinner fa-spin" v-if="loading"></i>
-                                      <template v-else>
-                                        <i class="fa fa-search" v-show="isEmpty"></i>
-                                        <i class="fa fa-times" v-show="isDirty" @click="reset"></i>
-                                      </template>
-                                    </span>
-                                    <!-- the input field -->
-                                    <input class="form-control"
-                                           type="text"
-                                           name="default_unit"
-                                           autocomplete="off"
-                                           v-model="query"
-                                           @keydown.down="down"
-                                           @keydown.up="up"
-                                           @keydown.enter.prevent="hit"
-                                           @blur="clear"
-                                           @input="update" />
-                                    <button  class="input-group-text"
-                                             role="button" href="#"
-                                             aria-haspopup="true"
-                                             aria-expanded="false"
-                                             @click.prevent="reload">
-                                      <i class="fa fa-caret-down"></i>
-                                    </button>
-                                    <!-- the list -->
-                                    {# Implementation note: If we use `d-block` instead of    #}
-                                    {# `style="display: block;"` the dropdown shows even when #}
-                                    {# `hasItems` is `False`.                                 #}
-                                    <ul class="dropdown-menu nav-pills p-1"
-                                        style="display: block;"
-                                        v-show="hasItems">
-                                      <li v-for="(item, $item) in items" class="nav-item"
-                                          @mousedown="hit"
-                                          @mousemove="setActive($item)">
-                                        <a :class="'nav-link' + activeClass($item)" v-text="item.title"></a>
-                                      </li>
-                                    </ul>
-                                  </div>
+                                    <div>
+                                      <div class="input-group mb-3">
+                                        <!-- optional indicators -->
+                                        <span class="input-group-text">
+                                          <i class="fa fa-spinner fa-spin" v-if="loading"></i>
+                                          <template v-else>
+                                            <i class="fa fa-search" v-show="isEmpty"></i>
+                                            <i class="fa fa-times" v-show="isDirty" @click="reset"></i>
+                                          </template>
+                                        </span>
+                                        <!-- the input field -->
+                                        <input class="form-control"
+                                               type="text"
+                                               name="default_unit"
+                                               autocomplete="off"
+                                               v-model="query"
+                                               @keydown.down="down"
+                                               @keydown.up="up"
+                                               @keydown.enter.prevent="hit"
+                                               @blur="clear"
+                                               @input="update" />
+                                        <button  class="input-group-text"
+                                                 role="button" href="#"
+                                                 aria-haspopup="true"
+                                                 aria-expanded="false"
+                                                 @click.prevent="reload">
+                                          <i class="fa fa-caret-down"></i>
+                                        </button>
+                                        <!-- the list -->
+                                        {# Implementation note: If we use `d-block` instead of    #}
+                                        {# `style="display: block;"` the dropdown shows even when #}
+                                        {# `hasItems` is `False`.                                 #}
+                                        <ul class="dropdown-menu nav-pills p-1"
+                                            style="display: block;"
+                                            v-show="hasItems">
+                                          <li v-for="(item, $item) in items" class="nav-item"
+                                              @mousedown="hit"
+                                              @mousemove="setActive($item)">
+                                            <a :class="'nav-link' + activeClass($item)" v-text="item.title"></a>
+                                          </li>
+                                        </ul>
+                                      </div>
+                                    </div>
+                                  </default-unit-typeahead>
+                                  <select class="form-control form-select"
+                                          v-model="entry.required"
+                                          @change="updateElement(entry, index)">
+                                    <option value="true">{% trans %}Required{% endtrans %}</option>
+                                    <option value="false">{% trans %}Optional{% endtrans %}</option>
+                                  </select>
                                 </div>
-                              </default-unit-typeahead>
-                              <select class="form-control form-select"
-                                      v-model="entry.required"
-                                      @change="updateElement(entry, index)">
-                                <option value="true">{% trans %}Required{% endtrans %}</option>
-                                <option value="false">{% trans %}Optional{% endtrans %}</option>
-                              </select>
-                              </div>
                                 <a class="title"
                                    :href="entry.url ? entry.url : '{{urls.pages_editables_index}}' + entry.slug + '/'"
                                    target="_blank"
@@ -214,8 +213,8 @@
                                   [[entry.title]]
                                 </a>
                                 <div class="title"
-                                  @click.prevent="toggleShowHide(entry, index)"
-                            v-if="index != activeIndex && !isPractice(entry)" >
+                                     @click.prevent="toggleShowHide(entry, index)"
+                                     v-if="index != activeIndex && !isPractice(entry)" >
                                   <div class="d-inline-block">
                                     <img class="icon-heading" :src="entry.picture" />
                                   </div>
@@ -230,13 +229,22 @@
                                     <i class="fa fa-plus"></i> Add
                                   </button>
                                   <button class="btn btn-link text-danger delete"
-                                      href=""
-                                      @click="setActive(entry, index)"
-                                      data-bs-toggle="modal" data-bs-target="#confirm-delete"
-                                      data-toggle="modal" data-target="#confirm-delete">
+                                          href=""
+                                          @click="setActive(entry, index)"
+                                          data-bs-toggle="modal" data-bs-target="#confirm-delete"
+                                          data-toggle="modal" data-target="#confirm-delete">
                                     <i class="fa fa-trash-o"></i>
                                   </button>
                                 </div>
+
+                                <div class="input-group"
+                                     v-if="addBelowIndex >= 0 && addBelowIndex == index">
+                                  <input class="form-control" type="text" v-model="activeEntry.title" />
+                                  <div class="input-group-btn">
+                                    <button class="btn btn-primary" type="submit" value="submit" @click="addElementBelow(entry, index)"><i class="fa fa-plus"></i> Add</button>
+                                  </div>
+                                </div>
+
                               </div>
                             </td>
                             <td>
@@ -281,19 +289,6 @@
                                   <i class="fa fa-plus"></i> Add [[entry.title]]
                                 </button>
                               </div>
-                            </td>
-                          </tr>
-                          <tr v-if="addBelowIndex >= 0 && addBelowIndex == index">
-                            <td></td>
-                            <td>
-                              <div class="input-group">
-                                <input class="form-control" type="text" v-model="activeEntry.title" />
-                                <div class="input-group-btn">
-                                  <button class="btn btn-primary" type="submit" value="submit" @click="addElementBelow(entry, index)"><i class="fa fa-plus"></i> Add</button>
-                                </div>
-                              </div>
-                            </td>
-                            <td :colspan="segments.length">
                             </td>
                           </tr>
                         </tbody>
@@ -454,6 +449,9 @@ jQuery(document).ready(function($) {
     $("#upload-btn").djupload({
         csrfToken: "{{csrf_token}}",
         uploadUrl: "{{urls.api_upload}}",
+        uploadSuccess: function() {
+            app.$forceUpdate();
+        },
         uploadSuccessMessage: function(filename, location) {
             return '{% trans %}"${filename}" uploaded sucessfully{% endtrans %}'.replace('${filename}', filename);
         }

djaopsp/views/content.py
@@ -80,8 +80,8 @@ class EditablesDetailView(VisibilityMixin, AccountMixin,
         # instead of relying on Vue to load the content. This is because
         # there seems to still be some issues with editors on
         # `.editable .edit-formatted`.
-        context.update({'element': self.element})
         context.update({
+            'element': self.element,
             'edit_perm': (self.manages(self.element.account) or
                 self.manages_broker)
         })

commit 62fc23d61936e25a9d186e56813af6f9a3a8347a

fixes `make initdb` error introduced by commit 8d8ef456

djaopsp/api/campaigns.py
@@ -371,7 +371,6 @@ class CampaignEditableContentAPIView(CampaignContentMixin,
 
     serializer_class = ContentNodeSerializer
     strip_segment_prefix = True
-    default_unit = Unit.objects.get(slug='freetext')
 
     def get_serializer_class(self):
         if self.request.method.lower() == 'post':
@@ -478,6 +477,7 @@ class CampaignEditableContentAPIView(CampaignContentMixin,
         #pylint:disable=too-many-locals
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+        self.default_unit = Unit.objects.get(slug='freetext')
 
         with transaction.atomic():
             parent = None
@@ -655,7 +655,6 @@ class CampaignEditableQuestionAPIView(QuestionMixin, CampaignContentMixin,
         }
     """
     serializer_class = get_question_serializer()
-    default_unit = Unit.objects.get(slug='freetext')
 
     def get_serializer_class(self):
         if self.request.method.lower() in ('post',):
@@ -757,6 +756,7 @@ class CampaignEditableQuestionAPIView(QuestionMixin, CampaignContentMixin,
         content_model = get_content_model()
         enum_question_model = EnumeratedQuestions
         question_model = get_question_model()
+        self.default_unit = Unit.objects.get(slug='freetext')
 
         with transaction.atomic():
             content_data = {}