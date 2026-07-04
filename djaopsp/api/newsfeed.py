djaodjin/djaopsp

commit dab09662cfb499ce0ecbf976a306f89346b22288

uses same cards for newsfeed and assess redirect pages (#177)

djaopsp/api/newsfeed.py
@@ -1,15 +1,16 @@
-# Copyright (c) 2025, DjaoDjin inc.
+# Copyright (c) 2026, DjaoDjin inc.
 # see LICENSE.
 
 import logging
 from collections import OrderedDict
 
-from dateutil.relativedelta import relativedelta
 from django.contrib.auth import get_user_model
 from django.db import models
+from rest_framework.settings import api_settings
+from rest_framework.generics import get_object_or_404
 from pages.api.newsfeed import NewsFeedListAPIView as NewsfeedBaseAPIView
 from survey.helpers import datetime_or_now
-from survey.models import PortfolioDoubleOptIn
+from survey.models import Campaign, PortfolioDoubleOptIn
 from survey.utils import get_account_model
 
 from ..api.serializers import UserNewsSerializer
@@ -102,6 +103,8 @@ class NewsfeedAPIView(VisibilityMixin, NewsfeedBaseAPIView):
         }
     """
     account_url_kwarg = 'profile'
+    campaign_url_kwarg = 'campaign'
+    search_param = api_settings.SEARCH_PARAM
     serializer_class = UserNewsSerializer
     URL_PATH_SEP = "/"
 
@@ -140,16 +143,29 @@ class NewsfeedAPIView(VisibilityMixin, NewsfeedBaseAPIView):
             })
         return initial_data
 
-    def get_pending_requests(self, at_time=None):
+    ### `get_query_param` might be better in pages
+    def get_query_param(self, key, default_value=None):
+        try:
+            return self.request.query_params.get(key, default_value)
+        except AttributeError:
+            pass
+        return self.request.GET.get(key, default_value)
+
+    def get_pending_requests(self, show_all=False, at_time=None):
+        #pylint:disable=too-many-locals
         if not at_time:
             at_time = datetime_or_now()
 
         assessments = []
+        campaign_slug = self.kwargs.get(self.campaign_url_kwarg)
+        campaign_filtered = (get_object_or_404(
+            Campaign.objects.all(), slug=campaign_slug)
+            if campaign_slug else None)
         for account in self.accounts:
             by_campaigns = OrderedDict()
             # XXX `pending_for` will also include grants pending acceptance.
             requests = PortfolioDoubleOptIn.objects.pending_for(
-                account, at_time=at_time).exclude(
+                account, at_time=at_time, campaign=campaign_filtered).exclude(
                     models.Q(grantee__slug=account) &
                     models.Q(state=PortfolioDoubleOptIn.OPTIN_GRANT_INITIATED)
             ).order_by('campaign__title')
@@ -176,31 +192,36 @@ class NewsfeedAPIView(VisibilityMixin, NewsfeedBaseAPIView):
             # We asume it is a work-in-progress worthy to show in the newsfeed
             # when `updated_at > created_at`. Otherwise the user will have
             # to go through the "History" page to update the response.
-            candidates = get_latest_active_assessments(account).filter(
-                updated_at__gt=models.F('created_at')).exclude(
+            candidates = get_latest_active_assessments(
+                account, campaign=campaign_filtered).exclude(
                 campaign__slug__endswith='-verified') # XXX Ad-hoc exclude
                                                # of verification campaigns.
+            if not show_all:
+                candidates = candidates.filter(
+                    updated_at__gt=models.F('created_at'))
+
             for sample in candidates:
-                if not sample.campaign in by_campaigns:
-                    by_campaigns[sample.campaign] = \
+                campaign = sample.campaign
+                if not campaign in by_campaigns:
+                    by_campaigns[campaign] = \
                         self._get_pending_request_initial_data(
-                                sample.campaign, account=account)
+                                campaign, account=account)
                 # We would use `reverse('assess_index', args=(account, sample))`
                 # if the template was not written to always make a POST request.
-                by_campaigns[sample.campaign]['update_url'] = reverse(
+                by_campaigns[campaign]['update_url'] = reverse(
                     'assess_redirect', args=(account,))
                 latest_completed = get_latest_completed_assessment(account,
-                    campaign=sample.campaign)
+                    campaign=campaign)
                 if latest_completed:
-                    by_campaigns[sample.campaign]['last_completed_at'] = \
+                    by_campaigns[campaign]['last_completed_at'] = \
                         latest_completed.created_at
-                    if latest_completed.created_at > sample.campaign.updated_at:
+                    if latest_completed.created_at > campaign.updated_at:
                         # The profile is forced to update the response when
                         # the questionnaire was last completed before the
                         # questionnaire was updated.
-                        by_campaigns[sample.campaign]['share_url'] = reverse(
+                        by_campaigns[campaign]['share_url'] = reverse(
                             'share', args=(account, latest_completed))
-                    by_campaigns[sample.campaign]['respondents'] = \
+                    by_campaigns[campaign]['respondents'] = \
                         get_user_model().objects.filter(
                             answer__sample=sample).distinct()
 
@@ -213,16 +234,38 @@ class NewsfeedAPIView(VisibilityMixin, NewsfeedBaseAPIView):
             if len(self.accounts) == 1:
                 account = next(iter(self.accounts))
             by_campaigns = OrderedDict()
-            campaign_candidates = get_campaign_candidates(
+            if campaign_filtered:
+                campaign_candidates = [campaign_filtered]
+            else:
+                campaign_candidates = get_campaign_candidates(
                     accounts=self.accessible_profiles,
                     tags=(set(['public']) | {plan['slug']
                         for plan in self.get_accessible_plans(self.request)
-            }))
+                }))
             for campaign in campaign_candidates:
                 if not campaign in by_campaigns:
                     by_campaigns[campaign] = \
                         self._get_pending_request_initial_data(
                             campaign, account=account)
+                # We would use `reverse('assess_index', args=(account, sample))`
+                # if the template was not written to always make a POST request.
+                by_campaigns[campaign]['update_url'] = reverse(
+                    'assess_redirect', args=(account,))
+                latest_completed = get_latest_completed_assessment(account,
+                    campaign=campaign)
+                if latest_completed:
+                    by_campaigns[campaign]['last_completed_at'] = \
+                        latest_completed.created_at
+                    if latest_completed.created_at > campaign.updated_at:
+                        # The profile is forced to update the response when
+                        # the questionnaire was last completed before the
+                        # questionnaire was updated.
+                        by_campaigns[campaign]['share_url'] = reverse(
+                            'share', args=(account, latest_completed))
+                    by_campaigns[campaign]['respondents'] = \
+                        get_user_model().objects.filter(
+                            answer__sample=latest_completed).distinct()
+
             assessments += by_campaigns.values()
 
         return assessments
@@ -234,5 +277,9 @@ class NewsfeedAPIView(VisibilityMixin, NewsfeedBaseAPIView):
         return context
 
     def get_queryset(self):
-        return list(self.get_pending_requests()) + list(
-            self.get_updated_elements())
+        search_term = self.get_query_param(self.search_param)
+        show_all = bool(search_term == 'requests')
+        results = list(self.get_pending_requests(show_all=show_all))
+        if search_term != 'requests':
+            results += list(self.get_updated_elements())
+        return results

djaopsp/templates/app/assess/redirects.html
@@ -1,93 +1,20 @@
 {% extends "app/assess/base.html" %}
 
+{% block app_content_title %}
+{% trans %}Select a questionnaire ...{% endtrans %}
+{% endblock %}
+
 {% block assess_content %}
-<section id="redirects" class="container py-4">
-  <h1 class="text-center">{% trans %}Select a questionnaire ...{% endtrans %}</h1>
-  <div class="bg-body text-body" style="min-height:30rem;">
-    <div class="row">
-      <div class="col-12 offset-md-3 col-md-6 my-4 py-4">
-        {% for assessment in candidates %}
-        <div class="card mb-4">
-          <div class="card-header">
-            <h5>{{assessment.title}}</h5>
-          </div>
-          <div class="card-body">
-            {% if assessment.descr %}
-            <p>
-{{assessment.descr|md}}
-            </p>
-            {% endif %}
-            {% if assessment.last_completed_at %}
-            <p>
-A response from <em>{{profile.printable_name}}</em> to this questionnaire was last completed on {{assessment.last_completed_at|humanizeDate}}.{% if assessment.respondents %} If you have questions, please reach out to the following co-worker(s): {% for user in assessment.respondents %} <span class="author"><a href="mailto:{{user.email}}">{{user.get_full_name()}}</a></span>{{ ", " if not loop.last else "" }}{% endfor %}.{% endif %}
-            </p>
-            {% endif %}
-            {% if assessment.requests %}
-            {% if assessment.ends_at %}
-            <p>
-{% trans profile=profile.printable_name, ends_at=assessment.ends_at|humanizeDate %}The following organizations would like a copy of the up-to-date response before {{ends_at}}.{% endtrans %}
-            </p>
-            {% else %}
-            <p>
-{% trans profile=profile.printable_name %}The following organizations would like a copy of the up-to-date response.{% endtrans %}
-            </p>
-            {% endif %}
-            <decorate-profiles data-elements='{{assessment.requests|to_json}}' inline-template>
-              <div>
-                {% for request in assessment.requests %}
-                <div>
-                  <div class="row align-items-center">
-                    <div class="col-2">
-                      <img class="img-fluid" style="max-height:64px;" :src="getAccountPicture('{{request.grantee}}') ? getAccountPicture('{{request.grantee}}') : '{{'/static/img/default-organization.png'|asset}}'" alt="{% trans %}Profile picture{% endtrans %}">
-                    </div>
-                    <div class="col-5">
-                      [[getAccountPrintableName('{{request.grantee}}') ? getAccountPrintableName('{{request.grantee}}') : '{{request.grantee}}']]
-                      <div>
-                        <small>on {{request.created_at|humanizeDate}}
-                          ({{request.created_at|humanizeTimeDelta}})</small>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                {% endfor %}
-                <p class="mt-2">
-<em>{% trans %}If you have completed the questionnaire more than 6 months ago, please kindly update the response to reflect current information.{% endtrans %}</em>
-                </p>
-              </div>
-            </decorate-profiles>
-            {% endif %}
-          </div>{# /.card-body #}
-          <div class="card-footer">
-            <div class="row">
-              <div class="col-md-6 text-center">
-                {% if assessment.share_url %}
-                <a class="btn btn-primary my-2"
-                   href="{{assessment.share_url}}">{% trans created_at=assessment.last_completed_at|humanizeDate %}Share {{created_at}} response{% endtrans %}</a>
-                {% endif %}
-              </div>
-              <div class="col-md-6 text-center">
-                {% if assessment.update_url %}
-                <a id="{{assessment.slug}}" class="btn btn-primary my-2"
-                   href="{{assessment.update_url}}">{% if assessment.share_url %}{% trans %}Update response{% endtrans %}{% else %}{% trans %}Continue{% endtrans %}{% endif %}</a>
-                {% else %}
-                <form action="." method="post">
-                  <input type="hidden" name="csrfmiddlewaretoken" value="{{csrf_token}}">
-                  <button id="{{assessment.slug}}" type="submit" class="btn btn-primary" name="campaign" value="{{assessment.slug}}" role="menuitem" tabindex="-1">{% trans %}Get started{% endtrans %} &raquo;</button>
-                </form>
-                {% endif %}
-              </div>
-            </div>
-          </div>{# /.card-footer #}
-        </div>
-        {% endfor %}
-        <hr class="mt-4" />
-        <p class="mt-4">
+<div id="redirects" class="container">
+  <h1 class="text-center py-4">{% trans %}Select a questionnaire ...{% endtrans %}</h1>
+  <div class="offset-sm-1 offset-md-2 offset-lg-3 col-sm-10 col-md-8 col-lg-6">
+    {% include "app/_newsfeed.html" %}
+  </div>
+  <hr class="mt-4" />
+  <p class="mt-4">
 {% trans %}This Website provides additional questionnaires on which to assess
 your organization's performance on a variety of qualitative and quantitative
 metrics.{% endtrans %} <a href="{{'docs/questionnaires/'|site_url}}">{% trans %}See questionnaires{% endtrans %} &raquo;</a>
         </p>
-      </div>
-    </div>
-  </div>
-</section>
+</div>{# .container #}
 {% endblock %}

djaopsp/templates/app/index.html
@@ -4,137 +4,7 @@
 <div id="newsfeed" class="container">
   <h1 class="text-center py-4">{% trans %}LEARN. ASSESS. IMPROVE.{% endtrans %}</h1>
   <div class="offset-sm-1 offset-md-2 offset-lg-3 col-sm-10 col-md-8 col-lg-6">
-  <newsfeed inline-template>
-    <div>
-      <div class="text-center" v-show="!itemsLoaded">
-        <p>
-          <i class="fa-solid fa-spinner fa-spin fa-2x"></i>
-        </p>
-        <p>
-{% trans %}Please wait...{% endtrans %}
-        </p>
-        <small>
-{% trans %}(if loading takes forever, try to force reload the page, i.e. press Ctrl + Shift + R){% endtrans %}
-        </small>
-      </div>
-      <div v-show="itemsLoaded && items.results.length == 0" v-cloak>
-        <div class="card mb-4">
-          <div class="card-header">
-            <div class="row align-items-center">
-              <div class="col-2">
-                <img class="img-fluid" style="max-height:64px;" src="{{'/static/img/default-organization.png'|asset}}" alt="{% trans %}Profile logo{% endtrans %}">
-              </div>
-              <div class="col-5">
-                <h2 class="card-title">{% trans %}Assess{% endtrans %}</h2>
-                <h3 class="card-subtitle">{% trans %}Pick a questionnaire{% endtrans %}</h3>
-              </div>
-            </div>
-          </div>
-          <div class="card-body">
-{% trans %}If you were prompted to take, or update, an assessment, start here.{% endtrans %}
-          </div> {# /.card-body #}
-          <div class="card-footer">
-            <div class="row">
-              <div class="offset-md-6 col-md-6 text-end">
-                <a id="getstarted" href="{{urls.getstarted}}" class="btn btn-primary">{% trans %}Get started{% endtrans %}</a>
-              </div>
-            </div>
-          </div> {# /.card-footer #}
-        </div>
-      </div>
-      <div id="newsfeed" v-show="itemsLoaded && items.results.length > 0" v-cloak>
-        <div class="card mb-4" v-for="assessment in items.results">
-          <div class="card-header">
-            <div class="row align-items-center">
-              <div class="col-2">
-                <img class="img-fluid" style="max-height:64px;" :src="getAccountPicture(assessment.account) ? getAccountPicture(assessment.account) : '{{'/static/img/default-organization.png'|asset}}'" alt="{% trans %}Profile logo{% endtrans %}">
-              </div>
-              <div class="col-10">
-                <h2 class="card-title" v-if="getAccountPrintableName(assessment.account) || assessment.account">[[getAccountPrintableName(assessment.account) ? getAccountPrintableName(assessment.account) : assessment.account]]</h2>
-                <h3  class="card-subtitle">[[assessment.title]]</h3>
-              </div>
-            </div>
-          </div>
-          <div class="card-body">
-            <div class="row" v-if="assessment.picture">
-              <div class="col-4">
-                <img class="img-fluid" :src="assessment.picture" alt="{% trans %}Profile logo{% endtrans %}" />
-              </div>
-              <div class="col-8"
-                   v-html="assessment.descr ? $globals.md(assessment.descr) : ''">
-              </div>
-            </div>
-            <p v-html="assessment.descr ? $globals.md(assessment.descr) : ''"
-               v-if="!assessment.picture">
-            </p>
-            <p v-if="assessment.last_completed_at">
-This assessment was last completed on [[$globals.humanizeDate(assessment.last_completed_at)]]
-<span v-if="assessment.respondents && assessment.respondents.length > 0">
-by one of your following co-worker(s):
-  <span v-for="(user, index) in assessment.respondents">
-    <span class="author" v-if="index == 0"><a :href="'mailto:' + getAccountField(user, 'email')">[[getAccountPrintableName(user) ? getAccountPrintableName(user) : user]]</a></span>
-    <span class="author" v-if="index >= 1">, <a :href="'mailto:' + getAccountField(user, 'email')">[[getAccountPrintableName(user) ? getAccountPrintableName(user) : user]]</a></span>
-  </span>
-</span>.
-            </p>
-            <div v-if="assessment.grantees && assessment.grantees.length > 0">
-              <p v-if="assessment.ends_at">
-{% trans ends_at='[[$globals.humanizeDate(assessment.ends_at)]]' %}The following organizations would like a copy of the up-to-date response before {{ends_at}}.{% endtrans %}
-              </p>
-              <p v-if="!assessment.ends_at">
-{% trans %}The following organizations would like a copy of the up-to-date response.{% endtrans %}
-              </p>
-              <decorate-profiles :elements="assessment.grantees" inline-template>
-                <div>
-                  <div v-for="request in elements">
-                    <div class="row align-items-center">
-                      <div class="col-2">
-                        <img class="img-fluid" style="max-height:64px;" :src="getAccountPicture(request.grantee) ? getAccountPicture(request.grantee) : '{{'/static/img/default-organization.png'|asset}}'" alt="{% trans %}Profile logo{% endtrans %}">
-                      </div>
-                      <div class="col-5">
-                        [[getAccountPrintableName(request.grantee) ? getAccountPrintableName(request.grantee) : request.grantee]]
-                        <div>
-                          <small>on [[$globals.humanizeDate(request.created_at)]]
-                            ([[$globals.humanizeTimeDelta(request.created_at)]])</small>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <p class="mt-2">
-<em>{% trans %}If you have completed the questionnaire more than 6 months ago, please kindly update the response to reflect current information.{% endtrans %}</em>
-                  </p>
-                </div>
-              </decorate-profiles>
-            </div>{# /assessment.grantees #}
-          </div>{# /.card-body #}
-          <div class="card-footer" v-if="assessment.update_url || assessment.external_url">
-            <div class="row">
-              <div class="col-md-6 text-center">
-                <a class="btn btn-primary"
-                   :href="assessment.share_url"
-                   v-if="assessment.share_url">{% trans created_at='[[$globals.humanizeDate(assessment.last_completed_at)]]' %}Share {{created_at}} response{% endtrans %}</a>
-              </div>
-              <div class="col-md-6 text-center">
-                <form :id="assessment.account + '-' + assessment.slug" :action="assessment.update_url" method="post" v-if="assessment.update_url">
-                  <input type="hidden" name="csrfmiddlewaretoken" value="{{csrf_token}}">
-                  <button class="btn btn-primary"
-                          type="submit"
-                          name="campaign"
-                          :value="assessment.slug"
-                          tabindex="-1">
-                    <span v-if="assessment.last_completed_at">{% trans %}Update response{% endtrans %}</span>
-                    <span v-if="!assessment.last_completed_at && assessment.update_url && assessment.update_url.indexOf('/getstarted/') < 0">{% trans %}Continue{% endtrans %}</span>
-                    <span v-if="!assessment.last_completed_at && assessment.update_url && assessment.update_url.indexOf('/getstarted/') >= 0">{% trans %}Get started{% endtrans %} &raquo;</span>
-                  </button>
-                </form>
-                <a class="btn btn-outline-primary" href="" @click.prevent="redirectToExternalUrl(assessment.external_url)" v-if="assessment.external_url">{% trans %}Learn about this product{% endtrans %}</a>
-              </div>
-            </div>
-          </div>{# /.card-footer #}
-        </div>
-      </div>
-    </div>
-  </newsfeed>
+    {% include "app/_newsfeed.html" %}
   </div>
   {% include "app/_external_redirect_modal.html" %}
 </div>{# .container #}

commit cd18d87afbd8845704db57def95a100cda791c72

restores all available questionnaires on /getstarted/ page
regression introduced by commit dab09662

djaopsp/api/newsfeed.py
@@ -231,21 +231,29 @@ class NewsfeedAPIView(VisibilityMixin, NewsfeedBaseAPIView):
 
             assessments += by_campaigns.values()
 
-        if not assessments:
-            # THere are no pending requests, we will add candidates
-            # so default questionnaires shows up.
+        if show_all or not assessments:
+            # There are no pending requests or we decided to show all
+            # questionnaires available to a user/profile regardless.
+            # This insures the default questionnaires shows up.
             account = None
+            by_campaigns = OrderedDict()
             if len(self.accounts) == 1:
                 account = next(iter(self.accounts))
-            by_campaigns = OrderedDict()
+            campaign_candidates = []
             if campaign_filtered:
-                campaign_candidates = [campaign_filtered]
+                found = False
+                for assessment in assessments:
+                    if assessment.get('slug') == campaign_filtered.slug:
+                        found = True
+                if not found:
+                    campaign_candidates = [campaign_filtered]
             else:
                 campaign_candidates = get_campaign_candidates(
                     accounts=self.accessible_profiles,
                     tags=(set(['public']) | {plan['slug']
                         for plan in self.get_accessible_plans(self.request)
-                }))
+                })).exclude(slug__in=[assessment.get('slug')
+                    for assessment in assessments])
             for campaign in campaign_candidates:
                 if not campaign in by_campaigns:
                     by_campaigns[campaign] = \
@@ -343,5 +351,5 @@ class GetStartedAPIView(NewsfeedAPIView):
     """
 
     def get_queryset(self):
-        results = list(self.get_pending_requests())
+        results = list(self.get_pending_requests(show_all=True))
         return results

djaopsp/utils.py
@@ -291,14 +291,17 @@ def get_latest_active_assessments(account, campaign=None):
             kwargs.update({'campaign': campaign})
         else:
             kwargs.update({'campaign__slug': campaign})
+    else:
+        kwargs.update({'answers__isnull': False})
     if isinstance(account, get_account_model()):
         kwargs.update({'account': account})
     else:
         kwargs.update({'account__slug': str(account)})
-    return Sample.objects.filter(
+    queryset = Sample.objects.filter(
         is_frozen=False, extra__isnull=True,
         **kwargs).order_by('-created_at').select_related(
-            'campaign', 'account')
+            'campaign', 'account').distinct()
+    return queryset
 
 
 def get_latest_completed_assessment(account, campaign=None):