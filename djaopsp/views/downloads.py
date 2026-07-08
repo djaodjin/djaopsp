4e3d6035268cea5ede27918823dcee47e98813f4
Sebastien Mirolo <smirolo@djaodjin.com>
2026-07-08 12:30:58 -0700

    fixes print of planned practices

--- djaopsp/views/downloads.py
+++ djaopsp/views/downloads.py
@@ -8,8 +8,13 @@ from survey.api.base import QuestionListAPIView
 
 from ..api.samples import AssessmentContentMixin
 
+class ImproveContentBaseView(QuestionListAPIView):
 
-class ImproveContentPDFView(AssessmentContentMixin, QuestionListAPIView):
+    def get_context_data(self, **kwargs):
+        return {}
+
+
+class ImproveContentPDFView(AssessmentContentMixin, ImproveContentBaseView):
 
     http_method_names = ['get']
     template_name = 'app/prints/improve.html'
@@ -24,13 +29,25 @@ class ImproveContentPDFView(AssessmentContentMixin, QuestionListAPIView):
         #pylint:disable=attribute-defined-outside-init
         self.object_list = []
         for item in self.get_queryset():
+            found = False
             if 'default_unit' in item:
+                planned = item.get('planned')
+                if planned:
+                    for answer in planned:
+                        unit = answer.get('unit')
+                        if (unit.slug == 'assessment' and
+                            answer.get('measured') in ['Yes', 'Mostly Yes']):
+                            found = True
+            if found:
                 element_text_qs = PageElement.objects.filter(
                     slug=item['slug']).values_list('text', flat=True)
                 element_text = element_text_qs[0] if element_text_qs else ""
                 item.update({'text': element_text})
                 self.object_list += [item]
-            self.table_of_content += [item]
+                self.table_of_content += [item]
         context = self.get_context_data(**kwargs)
-        context.update({'table_of_content': self.table_of_content})
+        context.update({
+            'table_of_content': self.table_of_content,
+            'object_list': self.object_list
+        })
         return PdfTemplateResponse(request, self.template_name, context)
